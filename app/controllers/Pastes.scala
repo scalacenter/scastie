8dc7551000db41a210e369c67b9a5f96049cc968
OlegYch <olegych@tut.by>
2012-08-26 03:13:38 +0300

    deployed on Sun Aug 26 03:13:37 AST 2012

--- app/controllers/Pastes.scala
+++ app/controllers/Pastes.scala
@@ -3,6 +3,7 @@ package controllers
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
+import scala.sys.process._
 
 
 object Pastes extends Controller {
@@ -14,10 +15,15 @@ object Pastes extends Controller {
   )
 
   def add = Action { implicit request =>
+    val res = sbt("hello").lines_!.toList.mkString("\n")
     Redirect(routes.Pastes.show("111"))
-        .flashing("paste" -> ("hello" + pasteForm.bindFromRequest().apply("paste").value))
+        .flashing("paste" -> (res + pasteForm.bindFromRequest().apply("paste").value))
   }
 
+
+  def sbt(command: String): String = {
+    (if (org.apache.commons.lang.SystemUtils.IS_OS_WINDOWS) "xsbt.cmd " else "xsbt.sh ") + command
+  }
   def show(id: String) = Action { implicit request =>
     Ok(views.html.index(id + " " + request.flash.get("paste").getOrElse("") + " Pasted!"))
   }
