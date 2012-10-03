12c74e14b2dbaa5caf6504fb2b8a56b18a314043
OlegYch <olegych@tut.by>
2012-10-03 23:01:34 +0300

    fancier testcase

--- renderer-template/src/main/g8/src/main/scala/test.scala
+++ renderer-template/src/main/g8/src/main/scala/test.scala
@@ -1,11 +1,22 @@
 /***
+scalaVersion := "ololo"
+
 libraryDependencies ++= Seq(
   "net.databinder" % "dispatch-twitter_2.9.0-1" % "0.8.3",
   "net.databinder" % "dispatch-http_2.9.0-1" % "0.8.3"
 )
 */
-object test extends App {
-  println("hello")
-  new java.io.File("d:/hello").delete
-  println("done")
+
+import dispatch.{ json, Http, Request }
+import dispatch.twitter.Search
+import json.{ Js, JsObject }
+
+def process(param: JsObject) = {
+  val Search.text(txt)        = param
+  val Search.from_user(usr)   = param
+  val Search.created_at(time) = param
+
+  "(" + time + ")" + usr + ": " + txt
 }
+
+Http.x((Search("#scala") lang "en") ~> (_ map process foreach println))
