9c58d3db7189a7149f5caed55bdc5666a3e9006d
OlegYch <olegych@tut.by>
2012-10-03 23:23:22 +0300

    make runnable

--- renderer-template/src/main/g8/src/main/scala/test.scala
+++ renderer-template/src/main/g8/src/main/scala/test.scala
@@ -6,17 +6,19 @@ libraryDependencies ++= Seq(
   "net.databinder" % "dispatch-http_2.9.0-1" % "0.8.3"
 )
 */
+object Main extends App {
 
-import dispatch.{ json, Http, Request }
-import dispatch.twitter.Search
-import json.{ Js, JsObject }
+  import dispatch.{json, Http, Request}
+  import dispatch.twitter.Search
+  import json.{Js, JsObject}
 
-def process(param: JsObject) = {
-  val Search.text(txt)        = param
-  val Search.from_user(usr)   = param
-  val Search.created_at(time) = param
+  def process(param: JsObject) = {
+    val Search.text(txt) = param
+    val Search.from_user(usr) = param
+    val Search.created_at(time) = param
 
-  "(" + time + ")" + usr + ": " + txt
+    "(" + time + ")" + usr + ": " + txt
+  }
+
+  Http.x((Search("#scala") lang "en") ~> (_ map process foreach println))
 }
-
-Http.x((Search("#scala") lang "en") ~> (_ map process foreach println))
