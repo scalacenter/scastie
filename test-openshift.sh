02bab88fcb1ae3fa0fe4c4b7879064039634acf8
OlegYch <olegych@tut.by>
2012-10-05 17:16:32 +0300

    separate config for renderer

--- test-openshift.sh
+++ test-openshift.sh
@@ -2,10 +2,12 @@
 path=`pwd`
 win_dir=`cygpath -w "${path}" || echo "${path}"`
 
+export OPENSHIFT_APP_NAME=scastie
+#export OPENSHIFT_APP_NAME=scastierenderer1
 export OPENSHIFT_REPO_DIR=$path/
 export OPENSHIFT_INTERNAL_IP=localhost
 export OPENSHIFT_INTERNAL_PORT=8080
-export OPENSHIFT_LOG_DIR=$path/target/
+export OPENSHIFT_LOG_DIR=$path/openshift_data/
 export OPENSHIFT_DATA_DIR=$win_dir/openshift_data/
 .openshift/action_hooks/stop
 .openshift/action_hooks/start
