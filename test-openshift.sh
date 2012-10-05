8168154d01c28cd521086632efcead97837f6656
OlegYch <olegych@tut.by>
2012-10-05 15:10:17 +0300

    use xsbt-start-script-plugin for consistency

--- test-openshift.sh
+++ test-openshift.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-export OPENSHIFT_REPO_DIR=`pwd`/
+path=`pwd`
+win_dir=`cygpath -w "${path}" || echo "${path}"`
+
+export OPENSHIFT_REPO_DIR=$path/
 export OPENSHIFT_INTERNAL_IP=localhost
 export OPENSHIFT_INTERNAL_PORT=8080
+export OPENSHIFT_LOG_DIR=$path/target/
+export OPENSHIFT_DATA_DIR=$win_dir/openshift_data/
+.openshift/action_hooks/stop
 .openshift/action_hooks/start
