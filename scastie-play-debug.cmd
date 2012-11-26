fb75a575db1df94b96e344ae653a5657ac311684
OlegYch <olegych@tut.by>
2012-11-26 19:18:30 +0300

    use correct name for logback config

--- scastie-play-debug.cmd
+++ scastie-play-debug.cmd
@@ -1,3 +1,3 @@
 set SCRIPT_DIR=%~dp0
 set SBT_OPTS=-agentlib:jdwp=transport=dt_shmem,server=n,address=scastie-play-debug,suspend=y -Dsbt.log.noformat=true
-bash "%SCRIPT_DIR%/xsbt.sh" "run -Dlogger.resource=logging.xml"
+bash "%SCRIPT_DIR%/xsbt.sh" "run -Dlogger.resource=logback.xml"
