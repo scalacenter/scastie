ca9d3b015ce423a37ae9ec40ade459ee0b795422
OlegYch <olegych@tut.by>
2012-12-02 21:48:43 +0300

    bind to 80 on aws

--- test-openshift.sh
+++ test-openshift.sh
@@ -6,7 +6,9 @@ export OPENSHIFT_APP_NAME=scastie
 #export OPENSHIFT_APP_NAME=scastierenderer1
 export OPENSHIFT_REPO_DIR=$path/
 export OPENSHIFT_INTERNAL_IP=`hostname -i || echo localhost`
-export OPENSHIFT_INTERNAL_PORT=8080
+if [ -z ${OPENSHIFT_INTERNAL_PORT} ]; then
+  export OPENSHIFT_INTERNAL_PORT=8080
+fi
 export OPENSHIFT_LOG_DIR=$path/openshift_data/
 export OPENSHIFT_DATA_DIR=$win_dir/openshift_data/
 .openshift/action_hooks/stop
