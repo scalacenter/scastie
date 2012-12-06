7ae5ace6299aaf3c656fc651245db4b59cdc2a2c
OlegYch <olegych@tut.by>
2012-12-06 04:37:28 +0300

    runnable by default

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,7 +8,7 @@ object Application extends Controller {
 
   def index = Action { implicit request =>
     val message = request.flash.get("error").map(Html(_)).getOrElse(Html("Enter your code and hit Submit:"))
-    val paste = request.flash.get("paste").getOrElse("")
+    val paste = request.flash.get("paste").getOrElse("object Main extends App {\n  \n}")
     Ok(views.html.index(message, paste)).withCookies(Cookie("uid", uid, maxAge = Some(Int.MaxValue)))
   }
 
