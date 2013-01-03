e238b8a5b0ef8b6c58225f5f177cae4fac45de56
OlegYch <olegych@tut.by>
2013-01-03 05:00:29 +0300

    support other scala versions

--- renderer-template/src/main/g8/project/DefaultSettings.scala
+++ renderer-template/src/main/g8/project/DefaultSettings.scala
@@ -2,20 +2,17 @@ import com.typesafe.sbtscalariform.ScalariformPlugin._
 import scala.Predef._
 import scalariform.formatter.preferences._
 import sbt._
-import Keys._
+import sbt.Keys._
 
 object DefaultSettings {
+  val sxrModule = "org.scala-tools.sxr" % "sxr" % "0.2.8-SNAPSHOT"
   def apply: Seq[Setting[_]] = scalariformSettings ++ Seq(
     resolvers += Resolver.url("olegych-repo",
       url("https://bitbucket.org/olegych/mvn/raw/default/ivy2/"))(Resolver.ivyStylePatterns)
     , resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
-    , addSupportedCompilerPlugin("org.scala-tools.sxr" % "sxr" % "0.2.8-SNAPSHOT")(pluginVersions)
+    , addSupportedCompilerPlugin(sxrModule)(pluginVersions)
     , addSupportedCompilerPlugin("com.foursquare.lint" % "linter" % "0.1-SNAPSHOT")(pluginVersions)
-    , scalacOptions <+= (scalaSource in Compile) map {"-P:sxr:base-directory:" + _.getAbsolutePath}
-    , scalacOptions <+= baseDirectory map { base =>
-      val linkFile = base / "sxr.links"
-      "-P:sxr:link-file:" + linkFile.getAbsolutePath
-    }
+    , scalacOptions
     , ScalariformKeys.preferences := FormattingPreferences().
         setPreference(AlignParameters, true).
         setPreference(AlignSingleLineCaseStatements, true).
@@ -27,6 +24,23 @@ object DefaultSettings {
     , traceLevel := 1000
     , crossPaths := false
   )
+
+
+  def scalacOptions: Project.Setting[Task[Seq[String]]] = {
+    Keys.scalacOptions <++= (scalaSource in Compile, baseDirectory, scalaVersion) map {
+      (scalaSource, baseDirectory, scalaVersion) =>
+        val sxrOptions = if (sxrSupported(scalaVersion)) {
+          Seq(
+            "-P:sxr:base-directory:" + baseDirectory.getAbsolutePath,
+            "-P:sxr:link-file:" + (baseDirectory / "sxr.links").getAbsolutePath)
+        } else {
+          Nil
+        }
+        Seq("-deprecation", "-unchecked") ++ sxrOptions
+    }
+  }
+
+  def sxrSupported(scalaVersion: String) = pluginVersions.isDefinedAt(scalaVersion, sxrModule)
 
   val pluginVersions: PartialFunction[(String, ModuleID), ModuleID] = {
     case ("2.9.2", module) => module.cross(CrossVersion.full)
