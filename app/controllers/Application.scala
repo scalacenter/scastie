647a28bac03a6deea72000d437460f7c1a7b9382
OlegYch <olegych@tut.by>
2013-01-12 02:14:44 +0300

    cosmetics

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,11 +4,12 @@ import play.api.mvc._
 import play.api.templates.Html
 import util.Random
 import base.TemplatePastes
+import play.api.i18n.Messages
 
 object Application extends Controller {
 
   def index = Action { implicit request =>
-    val message = request.flash.get("error").map(Html(_)).getOrElse(Html("Enter your code and hit Submit:"))
+    val message = request.flash.get("error").map(Html(_)).getOrElse(Html(Messages("enter.code")))
     val paste = request.flash.get("paste").getOrElse(TemplatePastes.default.content.get)
     Ok(views.html.index(message, paste)).withCookies(Cookie("uid", uid, maxAge = Some(Int.MaxValue)))
   }
