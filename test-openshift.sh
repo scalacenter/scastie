a86a39abdefa8a47d2acea777c513948933cb5ce
OlegYch <olegych@tut.by>
2013-01-26 00:15:11 +0300

    use external ip to bind

--- test-openshift.sh
+++ test-openshift.sh
@@ -2,16 +2,12 @@
 path=`pwd`
 win_dir=`cygpath -w "${path}" || echo "${path}"`
 
-if [ -z ${OPENSHIFT_APP_NAME} ]; then
-  export OPENSHIFT_APP_NAME=scastie
-fi
-#export OPENSHIFT_APP_NAME=scastierenderer1
-export OPENSHIFT_REPO_DIR=$path/
-export OPENSHIFT_INTERNAL_IP=`hostname -i || echo 0.0.0.0`
-if [ -z ${OPENSHIFT_INTERNAL_PORT} ]; then
-  export OPENSHIFT_INTERNAL_PORT=8080
-fi
-export OPENSHIFT_LOG_DIR=$path/openshift_data/
-export OPENSHIFT_DATA_DIR=$win_dir/openshift_data/
+export OPENSHIFT_APP_NAME=${OPENSHIFT_APP_NAME:-scastie}
+export OPENSHIFT_REPO_DIR=${OPENSHIFT_REPO_DIR:-$path/}
+DEFAULT_HOST_NAME=`curl http://169.254.169.254/latest/meta-data/public-ipv4 || echo 0.0.0.0`
+export OPENSHIFT_INTERNAL_IP=${OPENSHIFT_INTERNAL_IP:-$DEFAULT_HOST_NAME}
+export OPENSHIFT_INTERNAL_PORT=${OPENSHIFT_INTERNAL_PORT:-8080}
+export OPENSHIFT_LOG_DIR=${OPENSHIFT_LOG_DIR:-$path/openshift_data/}
+export OPENSHIFT_DATA_DIR=${OPENSHIFT_DATA_DIR:-$win_dir/openshift_data/}
 .openshift/action_hooks/stop
 .openshift/action_hooks/start
