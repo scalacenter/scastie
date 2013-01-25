02a67567393a4d5a8f3ce0d760c0821c167e9850
OlegYch <olegych@tut.by>
2013-01-26 01:09:34 +0300

    don't try to contact amazon by default

--- test-openshift.sh
+++ test-openshift.sh
@@ -4,8 +4,7 @@ win_dir=`cygpath -w "${path}" || echo "${path}"`
 
 export OPENSHIFT_APP_NAME=${OPENSHIFT_APP_NAME:-scastie}
 export OPENSHIFT_REPO_DIR=${OPENSHIFT_REPO_DIR:-$path/}
-DEFAULT_HOST_NAME=`curl http://169.254.169.254/latest/meta-data/public-ipv4 || echo 0.0.0.0`
-export OPENSHIFT_INTERNAL_IP=${OPENSHIFT_INTERNAL_IP:-$DEFAULT_HOST_NAME}
+export OPENSHIFT_INTERNAL_IP=${OPENSHIFT_INTERNAL_IP:-0.0.0.0}
 export OPENSHIFT_INTERNAL_PORT=${OPENSHIFT_INTERNAL_PORT:-8080}
 export OPENSHIFT_LOG_DIR=${OPENSHIFT_LOG_DIR:-$path/openshift_data/}
 export OPENSHIFT_DATA_DIR=${OPENSHIFT_DATA_DIR:-$win_dir/openshift_data/}
