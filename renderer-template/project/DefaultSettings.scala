ef3f3eec96b22318cd423bbcff8f7322a341e3b6
OlegYch <olegych@tut.by>
2013-06-02 22:23:52 +0300

    update sxr

--- renderer-template/project/DefaultSettings.scala
+++ renderer-template/project/DefaultSettings.scala
@@ -4,12 +4,12 @@ import sbt._
 import sbt.Keys._
 
 object DefaultSettings {
-  val sxrModule = "org.scala-tools.sxr" % "sxr" % "0.2.8-SNAPSHOT"
+  val sxrModule = "org.scala-sbt.sxr" %% "sxr" % "0.3.0"
   def apply: Seq[Setting[_]] = scalariformSettings ++ Seq(
     resolvers += Resolver.url("olegych-repo",
       url("https://bitbucket.org/olegych/mvn/raw/default/ivy2/"))(Resolver.ivyStylePatterns)
     , resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
-    , addSupportedCompilerPlugin(sxrModule)(pluginVersions)
+    , addSupportedCompilerPlugin(sxrModule)(sxrVersion)
     , addSupportedCompilerPlugin("com.foursquare.lint" % "linter" % "0.1-SNAPSHOT")(pluginVersions)
     , scalacOptions
     , ScalariformKeys.preferences := FormattingPreferences().
@@ -26,7 +26,7 @@ object DefaultSettings {
   def scalacOptions: Project.Setting[Task[Seq[String]]] = {
     Keys.scalacOptions <++= (scalaSource in Compile, baseDirectory, scalaVersion) map {
       (scalaSource, baseDirectory, scalaVersion) =>
-        val sxrOptions = if (sxrSupported(scalaVersion)) {
+        val sxrOptions = if (sxrVersion.isDefinedAt(scalaVersion, sxrModule)) {
           Seq(
             "-P:sxr:base-directory:" + baseDirectory.getAbsolutePath,
             "-P:sxr:link-file:" + (baseDirectory / "sxr.links").getAbsolutePath)
@@ -47,16 +47,18 @@ object DefaultSettings {
 
   object Is210 extends IsStartsWith("2.10")
 
-  def sxrSupported(scalaVersion: String) = pluginVersions.isDefinedAt(scalaVersion, sxrModule)
-
+  val sxrVersion: PartialFunction[(String, ModuleID), ModuleID] = {
+    case (v, module) if Is29(v)  => module.copy(organization = "org.scala-tools.sxr", revision = "0.2.8-SNAPSHOT")
+    case (v, module) if Is210(v) => module
+  }
   val pluginVersions: PartialFunction[(String, ModuleID), ModuleID] = {
-    case (v, module) if Is29(v) => module.copy(name = module.name + "_2.9.2")
+    case (v, module) if Is29(v)  => module.copy(name = module.name + "_2.9.2")
     case (v, module) if Is210(v) => module.cross(CrossVersion.binary)
   }
 
   def addSupportedCompilerPlugin(module: ModuleID)
                                 (version: PartialFunction[(String, ModuleID), ModuleID]): Project.Setting[Seq[ModuleID]] =
-    libraryDependencies <++= (scalaVersion) { scalaVersion =>
+    libraryDependencies <++= scalaVersion { scalaVersion =>
       version.lift(scalaVersion, module).map(compilerPlugin(_)).toList
     }
 }
