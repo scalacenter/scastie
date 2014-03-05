a7fde458cb64543308b5cc12bca4414f862de04c
OlegYch <olegych@tut.by>
2014-03-06 02:44:27 +0300

    sh site is down, use webjars

--- project/ApplicationBuild.scala
+++ project/ApplicationBuild.scala
@@ -13,35 +13,39 @@ object ApplicationBuild extends Build {
     def scalaIo(module: String) = "com.github.scala-incubator.io" %% ("scala-io-" + module) % "0.4.2"
     Project(id = "renderer", base = file("renderer"),
       settings = Defaults.defaultSettings ++
-        play.Project.intellijCommandSettings("SCALA") ++
-        Seq(
-          Keys.scalaVersion := scalaVersion
-          , Keys.libraryDependencies ++= Seq(
-            "org.slf4j" % "jul-to-slf4j" % "1.6.6",
-            akka("actor"),
-            akka("remote"),
-            akka("slf4j"),
-            "com.typesafe" % "config" % "1.0.0",
-            "ch.qos.logback" % "logback-classic" % "1.0.7",
-            scalaIo("core"),
-            scalaIo("file"),
-            "org.apache.commons" % "commons-lang3" % "3.1",
-            "net.sourceforge.collections" % "collections-generic" % "4.01",
-            "org.scalaz" %% "scalaz-core" % "7.0.4",
-            "org.scala-lang" % "scala-compiler" % scalaVersion
+          play.Project.intellijCommandSettings("SCALA") ++
+          Seq(
+            Keys.scalaVersion := scalaVersion
+            , Keys.libraryDependencies ++= Seq(
+              "org.slf4j" % "jul-to-slf4j" % "1.6.6",
+              akka("actor"),
+              akka("remote"),
+              akka("slf4j"),
+              "com.typesafe" % "config" % "1.0.0",
+              "ch.qos.logback" % "logback-classic" % "1.0.7",
+              scalaIo("core"),
+              scalaIo("file"),
+              "org.apache.commons" % "commons-lang3" % "3.1",
+              "net.sourceforge.collections" % "collections-generic" % "4.01",
+              "org.scalaz" %% "scalaz-core" % "7.0.4",
+              "org.scala-lang" % "scala-compiler" % scalaVersion
+            )
           )
-        )
-        ++ SbtStartScript.startScriptForClassesSettings
-        ++ Seq(Keys.mainClass in Compile := Option("com.olegych.scastie.RendererMain"))
+          ++ SbtStartScript.startScriptForClassesSettings
+          ++ Seq(Keys.mainClass in Compile := Option("com.olegych.scastie.RendererMain"))
     )
   }
 
   val main = play.Project(appName, appVersion).settings(
     SbtStartScript.startScriptForClassesSettings ++
-      Seq(Keys.mainClass in Compile := Option("ProdNettyServer")
-        , Keys.scalaVersion := scalaVersion
-        , Keys.watchSources <++= Keys.baseDirectory map { path => ((path / "public") ** "*").get}
-      ): _*
+        Seq(Keys.mainClass in Compile := Option("ProdNettyServer")
+          , Keys.scalaVersion := scalaVersion
+          , Keys.watchSources <++= Keys.baseDirectory map { path => ((path / "public") ** "*").get}
+          , Keys.libraryDependencies ++= Seq(
+            "org.webjars" % "syntaxhighlighter" % "3.0.83"
+            , "org.webjars" %% "webjars-play" % "2.1.0-3"
+            , "org.webjars" % "bootstrap" % "3.1.0")
+        ): _*
   ) dependsOn renderer aggregate renderer
 
 }
