c3e4f660bf442ea3a201cd858d7b94f8218342d3
OlegYch <olegych@tut.by>
2014-07-08 17:20:38 +0300

    update to scala 2.11

--- project/ApplicationBuild.scala
+++ project/ApplicationBuild.scala
@@ -1,19 +1,18 @@
-import sbt._
 import com.typesafe.sbt.SbtStartScript
+import com.typesafe.sbt.web.Import._
+import sbt.Keys._
+import sbt._
 
 object ApplicationBuild extends Build {
-  val appName = "scastie"
-  val appVersion = "1.0"
-
-  val scalaVersion = "2.10.3"
-  val akkaVersion = "2.2.4"
+  val scalaVersion = "2.11.1"
+  val akkaVersion = "2.3.2"
 
   def logging(allDependencies: Seq[ModuleID]): Seq[ModuleID] = {
     Seq(
       "org.slf4j" % "slf4j-api" % "1.7.6"
       , "org.slf4j" % "jul-to-slf4j" % "1.7.6"
-      , "ch.qos.logback" % "logback-core" % "1.0.13" % Runtime
-      , "ch.qos.logback" % "logback-classic" % "1.0.13" % Runtime
+      , "ch.qos.logback" % "logback-core" % "1.1.1" % Runtime
+      , "ch.qos.logback" % "logback-classic" % "1.1.1" % Runtime
       , "org.slf4j" % "jcl-over-slf4j" % "1.7.6" % Runtime
       , "org.slf4j" % "log4j-over-slf4j" % "1.7.6" % Runtime
     ) ++
@@ -30,38 +29,31 @@ object ApplicationBuild extends Build {
     Keys.incOptions := Keys.incOptions.value.withNameHashing(true)
     , Keys.scalaVersion := scalaVersion
     , Keys.libraryDependencies ++= Seq(
-      "com.github.scala-incubator.io" %% "scala-io-file" % "0.4.2"
-      , "org.scalaz" %% "scalaz-core" % "7.0.6"
+      "com.github.scala-incubator.io" %% "scala-io-file" % "0.4.3-1"
+      , "org.scalaz" %% "scalaz-core" % "7.1.0-RC1"
     )
     , Keys.allDependencies ~= logging
   )
-  val renderer = {
-    def akka(module: String) = "com.typesafe.akka" %% ("akka-" + module) % akkaVersion
-    Project(id = "renderer", base = file("renderer"),
-      settings = Defaults.defaultSettings ++ defaultSettings ++
-        Seq(
-          Keys.libraryDependencies ++= Seq(
-            akka("actor"),
-            akka("remote"),
-            akka("slf4j"),
-            "org.apache.commons" % "commons-lang3" % "3.1",
-            "net.sourceforge.collections" % "collections-generic" % "4.01",
-            "org.scala-lang" % "scala-compiler" % scalaVersion
-          ))
-        ++ SbtStartScript.startScriptForClassesSettings
-        ++ Seq(Keys.mainClass in Compile := Option("com.olegych.scastie.RendererMain"))
-    )
-  }
+  def akka(module: String) = "com.typesafe.akka" %% ("akka-" + module) % akkaVersion
+  val renderer = project.settings(defaultSettings: _*).settings(SbtStartScript.startScriptForClassesSettings: _*).
+    settings(
+      Keys.mainClass in Compile := Option("com.olegych.scastie.RendererMain"),
+      Keys.libraryDependencies ++= Seq(
+        akka("actor"),
+        akka("remote"),
+        akka("slf4j"),
+        "org.apache.commons" % "commons-lang3" % "3.1",
+        "net.sourceforge.collections" % "collections-generic" % "4.01",
+        "org.scala-lang" % "scala-compiler" % scalaVersion
+      ))
 
-  val main = play.Project(appName, appVersion).settings(defaultSettings: _*).settings(
-    SbtStartScript.startScriptForClassesSettings ++
-      Seq(Keys.mainClass in Compile := Option("ProdNettyServer")
-        , Keys.watchSources <++= Keys.baseDirectory map { path => ((path / "public") ** "*").get}
-        , Keys.libraryDependencies ++= Seq(
-          "org.webjars" % "syntaxhighlighter" % "3.0.83"
-          , "org.webjars" %% "webjars-play" % "2.2.1-2"
-          , "org.webjars" % "bootstrap" % "3.1.0")
-      ): _*
-  ) dependsOn renderer aggregate renderer
-
+  val scastie = project.in(file(".")).enablePlugins(com.typesafe.sbt.web.SbtWeb, play.PlayScala).settings(defaultSettings: _*).
+    settings(SbtStartScript.startScriptForClassesSettings: _*).settings(
+      Keys.allDependencies ~= (_.map(_.exclude("com.typesafe.play", "play-doc_2.11").exclude("com.typesafe.play", "play-docs_2.11")))
+      , Keys.mainClass in Compile := Option("ProdNettyServer")
+      , (WebKeys.public in Assets) := (classDirectory in Compile).value / "public"
+      , (compile in Compile) <<= (compile in Compile).dependsOn(WebKeys.assets in Assets)
+      , Keys.libraryDependencies ++=
+    Seq("org.webjars" % "syntaxhighlighter" % "3.0.83", "org.webjars" %% "webjars-play" % "2.3.0", "org.webjars" % "bootstrap" % "3.1.0")
+    ) dependsOn renderer aggregate renderer
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,10 +1,5 @@
-// Comment to get more information during initialization
-logLevel := Level.Info
-
-// The Typesafe repository 
 resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
 
-// Use the Play sbt plugin for Play projects
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.2.2")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.3.1")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-start-script" % "0.10.0")
