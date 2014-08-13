1f89c579a7fbc2c1f101abbfdbecc2ed19113096
OlegYch <olegych@tut.by>
2014-08-14 02:15:04 +0300

    update templates to 2.11

--- app/base/TemplatePastes.scala
+++ app/base/TemplatePastes.scala
@@ -13,7 +13,7 @@ object TemplatePastes {
 
   val default = nextPaste( """
 /***
-scalaVersion := "2.10.3"
+scalaVersion := "2.11.2"
 
 */
 object Main extends App {
@@ -24,12 +24,30 @@ object Main extends App {
     List(
       "typelevel" -> nextPaste( """
 /***
-scalaVersion := "2.10.3"
+scalaVersion := "2.11.2"
 
-libraryDependencies ++= Seq("org.scalaz" %% "scalaz-core" % "7.0.6",
-                            "org.typelevel" %% "shapeless-spire" % "0.1.2",
-                            "org.typelevel" %% "shapeless-scalaz" % "0.1.2"
-                            )
+resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases"
+
+libraryDependencies ++= {
+  val scalazVersion = "7.1.0"
+  val scalazStreamVersion = "0.5a"
+  val shapelessVersion = "2.0.0"
+  val monocleVersion = "0.5.0"
+  val spireVersion = "0.8.2"
+  Seq(
+    "org.scalaz" %% "scalaz-iteratee" % scalazVersion,
+    "org.scalaz" %% "scalaz-effect" % scalazVersion,
+    "org.scalaz" %% "scalaz-typelevel" % scalazVersion,
+    "org.scalaz" %% "scalaz-scalacheck-binding" % scalazVersion,
+    "org.scalaz" %% "scalaz-concurrent" % scalazVersion,
+    "com.chuusai" %% "shapeless" % shapelessVersion,
+    "com.github.julien-truffaut" %% "monocle-generic" % monocleVersion,
+    "com.github.julien-truffaut" %% "monocle-law" % monocleVersion,
+    "com.github.julien-truffaut" %% "monocle-macro" % monocleVersion,
+    "org.spire-math" %% "spire" % spireVersion,
+    "org.scalaz.stream" %% "scalaz-stream" % scalazStreamVersion
+  )
+}
 */
 object Main extends App {
   import scalaz._, Scalaz._
@@ -45,20 +63,30 @@ object Main extends App {
   println(f1)
 
   import spire.math._ // provides functions, types, and type classes
+
   import spire.implicits._ // provides infix operators, instances and conversions
+
   import spire.random._
-  val rng = Cmwc5()
-  implicit val nextmap = Dist.map[Int, Complex[Double]](2, 5)
-  val m = rng.next[Map[Int, Complex[Double]]]
-  println(m)
+
+  import scalaz.stream._
+  import scalaz.concurrent.Task
+
+  import monocle._
+  import monocle.syntax._
+  import monocle.function.HeadOption._ // to use headOption (a polymorphic optic)
+
+  import monocle.std.string._ // to get String instance for HeadOption
+
+
 }
-                                """)
+
+        """)
       ,
       "typesafe" -> nextPaste( """
 /***
-scalaVersion := "2.10.3"
+scalaVersion := "2.11.2"
 
-libraryDependencies ++= Seq("com.typesafe.play" %% "play" % "2.2.2")
+libraryDependencies ++= Seq("com.typesafe.play" %% "play" % "2.3.3")
 */
 import play.api._
 import play.api.mvc._
@@ -78,13 +106,13 @@ sbtPlugin := true
 import sbt._
 import Keys._
 object Build extends Build {
-  val p = project.settings(Keys.scalaVersion := Keys.name.value)
+  val p = project.settings(scalaVersion := name.value)
 }
                           """)
       ,
-      "scala 2.9" -> nextPaste( """
+      "scala 2.10" -> nextPaste( """
 /***
-scalaVersion := "2.9.3"
+scalaVersion := "2.10.4"
 
 */
 object Main extends App {
