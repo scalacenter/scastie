ed44fe9dc740c85adcb46586105332b5e80438e2
OlegYch <olegych@tut.by>
2015-07-12 03:32:25 +0300

    faster redeploy

--- test-openshift.sh
+++ test-openshift.sh
@@ -8,5 +8,5 @@ export OPENSHIFT_INTERNAL_IP=${OPENSHIFT_INTERNAL_IP:-0.0.0.0}
 export OPENSHIFT_INTERNAL_PORT=${OPENSHIFT_INTERNAL_PORT:-8080}
 export OPENSHIFT_LOG_DIR=${OPENSHIFT_LOG_DIR:-$path/openshift_data/}
 export OPENSHIFT_DATA_DIR=${OPENSHIFT_DATA_DIR:-$win_dir/openshift_data/}
-.openshift/action_hooks/stop
+
 .openshift/action_hooks/start
