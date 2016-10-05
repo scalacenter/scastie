695543862767f991ac3eeedf21814d085db446f4
Guillaume Massé <masgui@gmail.com>
2016-10-05 13:58:17 +0200

    refactor frontend input/ouput in preparation for libraryDependencies

--- client/src/main/scala/client/Model.scala
+++ client/src/main/scala/client/Model.scala
@@ -1,5 +1,19 @@
 package client
 
+// input
+case class Inputs(
+  code: String = "",
+  libraries: Set[Library] = Set()
+)
+case class Library(groupId: String, artifactId: String, version: String)
+
+// outputs
+case class Outputs(
+  console: Vector[String] = Vector(),
+  compilationInfos: Set[api.Problem] = Set(),
+  instrumentations: Set[api.Instrumentation] = Set()
+)
+
 sealed trait Severity
 final case object Info extends Severity
 final case object Warning extends Severity
