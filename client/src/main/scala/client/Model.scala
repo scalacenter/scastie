61adadeeb11de2b6679fca26f79c0b979df725a6
Guillaume Massé <masgui@gmail.com>
2016-10-11 17:16:47 +0200

    basic libraries search
    based on scaladex cf026faf53bcebbe21070e68accf0104c12d7044

--- client/src/main/scala/client/Model.scala
+++ client/src/main/scala/client/Model.scala
@@ -1,7 +1,10 @@
 package client
 
 case class Project(
-  
+  organization: String,
+  repository: String,
+  logo: Option[String] = None,
+  artifacts: List[String] = Nil
 )
 
 // case class JvmDependency(mavenReference: MavenReference)
@@ -9,27 +12,69 @@ case class Project(
 
 case class MavenReference(groupId: String, artifactId: String, version: String)
 
-sealed trait ScalaVersion
-
-case class Dotty(version: Version)
-
 case class Version(_1: Int, _2: Int, _3: Int, extra: String = "") {
   def binary: String = s"${_1}.${_2}" // like 2.11
   override def toString: String = s"${_1}.${_2}.${_3}$extra"
 }
 
-sealed trait ScalaTarget
+sealed trait ScalaTargetType {
+  def logoFileName: String
+  def label: String
+  def scalaTarget: ScalaTarget
+}
+object ScalaTargetType {
+  case object JVM extends ScalaTargetType {
+    def logoFileName = "smooth-spiral.png"
+    def label = "Scala"
+    def scalaTarget = ScalaTarget.Jvm()
+  }
+  case object Dotty extends ScalaTargetType {
+    def logoFileName = "dotty3.svg"
+    def label = "Dotty"
+    def scalaTarget = ScalaTarget.Dotty
+  }
+  case object JS extends ScalaTargetType {
+    def logoFileName = "scala-js.svg"
+    def label = "Scala.Js"
+    def scalaTarget = ScalaTarget.Js()
+  }
+  // case object Native extends ScalaTargetType {
+  //   def logoFileName = "native2.png"
+  //   def label = "Native"
+  //   def scalaTarget = ScalaTarget.Native
+  // }
+}
+
+sealed trait ScalaTarget {
+  def targetType: ScalaTargetType
+  def scaladexRequest: Map[String, String]
+}
 object ScalaTarget {
   private val defaultScalaVersion = Version(2, 11, 8)
   private val defaultScalaJsVersion = Version(0, 6, 12)
 
-  case class Jvm(scalaVersion: Version = defaultScalaVersion) extends ScalaTarget
+  case class Jvm(scalaVersion: Version = defaultScalaVersion) extends ScalaTarget {
+    def targetType = ScalaTargetType.JVM
+    def scaladexRequest = Map("target" -> "JVM", "scalaVersion" -> scalaVersion.binary)
+  }
   case class Js(scalaVersion: Version = defaultScalaVersion, 
-                scalaJsVersion: Version = defaultScalaJsVersion) extends ScalaTarget
-  // case object Native extends ScalaTarget
-  case object Dotty extends ScalaTarget
-}
+                scalaJsVersion: Version = defaultScalaJsVersion) extends ScalaTarget {
 
+    def targetType = ScalaTargetType.JS
+    def scaladexRequest = Map(
+      "target" -> "JS",
+      "scalaVersion" -> scalaVersion.binary,
+      "scalaJsVersion" -> scalaJsVersion.binary
+    )
+  }
+  // case object Native extends ScalaTarget {
+  //   def targetType = ScalaTargetType.Native
+  // }
+  case object Dotty extends ScalaTarget {
+    def targetType = ScalaTargetType.Dotty
+    def scaladexRequest = Map()
+  }
+}
 
 // input
 case class Inputs(
--- client/src/main/scala/client/ScaladexSearch.scala
+++ client/src/main/scala/client/ScaladexSearch.scala
@@ -4,25 +4,77 @@ import App._
 
 import japgolly.scalajs.react._, vdom.all._
 
+import org.scalajs.dom
+import dom.ext.Ajax
+import scalajs.concurrent.JSExecutionContext.Implicits.queue
+
+import upickle.default.{read ⇒ uread}
+
 object ScaladexSearch {
 
-  // input
+  private[ScaladexSearch] case class SearchState(
+    query: String = "",
+    projects: List[Project] = Nil
+  )
 
-  // libs list
+  private[ScaladexSearch] class SearchBackend(scope: BackendScope[(State, Backend), SearchState]) {
+    def keyDown(e: ReactEventI) = {
+      Callback(())
+    }
+
+    def setQuery(e: ReactEventI) = {
+      def fetchProjects(queryIn: String, appState: State): Callback = { //: CallbackTo[List[Project]] = {
+        if(!queryIn.isEmpty) {
+          val query = 
+            (Map("q" -> queryIn) ++ appState.inputs.target.scaladexRequest).map{ 
+              case (k, v) => s"$k=$v" 
+            }.mkString("?", "&", "")
+
+          Callback.future(
+            Ajax.get("http://localhost:8080/api/scastie" + query).map{ ret =>
+              uread[List[Project]](ret.responseText)
+            }.map(projects => scope.modState(_.copy(projects = projects)))
+          )
+        } else scope.modState(_.copy(projects = Nil))
+      }
+
+      e.extract(_.target.value)(value =>
+        for {
+          _ <- scope.modState(_.copy(query = value))
+          props <- scope.props
+          (appState, _) = props
+          _ <- fetchProjects(value, appState)
+        } yield ()
+      )
+    }
+  }
 
   private val component = ReactComponentB[(State, Backend)]("Scaladex Search")
-    .render_P { case (state, backend) =>
-      // import backend._
-
-      
-
-
-
-      div(
-        // input
-        // libs list
+    .initialState(SearchState())
+    .backend(new SearchBackend(_))
+    .renderPS { case (scope, (state, backend), searchState) =>
+      li(`class` := "scaladex")(
+        fieldset(
+          legend("Scala Library"),
+          input.search(
+            placeholder := "Search for 'time'",
+            value := searchState.query,
+            onChange ==> scope.backend.setQuery,
+            onKeyDown ==> scope.backend.keyDown
+          ),
+          ul(searchState.projects.map{ case Project(organization, repository, logo, artifacts) =>
+            li(
+              logo.map(url => img(src := url, alt := "project logo")).getOrElse(EmptyTag),
+              span(s"$organization /$repository"),
+              ul(artifacts.map(artifact =>
+                li(artifact)
+              ))
+            )
+          })
+        )
       )
     }
     .build
+
   def apply(state: State, backend: Backend) = component((state, backend))
 }
