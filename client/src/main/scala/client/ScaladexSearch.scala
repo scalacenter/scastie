3b789af1804fb79eb5335c38c872c0563c7dff60
Guillaume Massé <masgui@gmail.com>
2016-10-11 17:58:04 +0200

    wip lib search

--- client/src/main/scala/client/ScaladexSearch.scala
+++ client/src/main/scala/client/ScaladexSearch.scala
@@ -18,27 +18,32 @@ object ScaladexSearch {
   )
 
   private[ScaladexSearch] class SearchBackend(scope: BackendScope[(State, Backend), SearchState]) {
+    def keyDown(e: ReactEventI) = {
+      Callback(())
+    }
+
     def setQuery(e: ReactEventI) = {
+      def fetchProjects(queryIn: String, appState: State): Callback = { //: CallbackTo[List[Project]] = {
+        if(!queryIn.isEmpty) {
+          val query = 
+            (Map("q" -> queryIn) ++ appState.inputs.target.scaladexRequest).map{ 
+              case (k, v) => s"$k=$v" 
+            }.mkString("?", "&", "")
 
-      def fetchProjects(searchState: SearchState, appState: State): Callback = { //: CallbackTo[List[Project]] = {
-        val query = 
-          (Map("q" -> searchState.query) ++ appState.inputs.target.scaladexRequest).map{ 
-            case (k, v) => s"$k=$v" 
-          }.mkString("?", "&", "")
-
-        Callback.future(
-          Ajax.get("http://localhost:8080/api/scastie" + query).map{ ret =>
-            uread[List[Project]](ret.responseText)
-          }.map(projects => scope.modState(_.copy(projects = projects)))
-        )
+          Callback.future(
+            Ajax.get("http://localhost:8080/api/scastie" + query).map{ ret =>
+              uread[List[Project]](ret.responseText)
+            }.map(projects => scope.modState(_.copy(projects = projects)))
+          )
+        } else scope.modState(_.copy(projects = Nil))
       }
+
       e.extract(_.target.value)(value =>
         for {
           _ <- scope.modState(_.copy(query = value))
           props <- scope.props
           (appState, _) = props
-          state <- scope.state
-          _ <- fetchProjects(state, appState)
+          _ <- fetchProjects(value, appState)
         } yield ()
       )
     }
@@ -54,11 +59,18 @@ object ScaladexSearch {
           input.search(
             placeholder := "Search for 'time'",
             value := searchState.query,
-            onChange ==> scope.backend.setQuery
+            onChange ==> scope.backend.setQuery,
+            onKeyDown ==> scope.backend.keyDown
           ),
-          ul(searchState.projects.map(p =>
-            li(p.toString)
-          ))
+          ul(searchState.projects.map{ case Project(organization, repository, logo, artifacts) =>
+            li(
+              logo.map(url => img(src := url, alt := "project logo")).getOrElse(EmptyTag),
+              span(s"$organization /$repository"),
+              ul(artifacts.map(artifact =>
+                li(artifact)
+              ))
+            )
+          })
         )
       )
     }
