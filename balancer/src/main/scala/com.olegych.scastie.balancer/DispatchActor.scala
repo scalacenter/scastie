735fac596d686c7d2d81f4321b11351a5b766a93
Guillaume Massé <masgui@gmail.com>
2017-02-10 19:40:10 +0100

    configs

--- balancer/src/main/scala/com.olegych.scastie.balancer/DispatchActor.scala
+++ balancer/src/main/scala/com.olegych.scastie.balancer/DispatchActor.scala
@@ -1,118 +1,114 @@
 package com.olegych.scastie
 package balancer
 
- import akka.actor.ActorSelection
+import api._
 
+import akka.actor.{Actor, ActorRef, ActorSelection}
+import akka.remote.DisassociatedEvent
+
+import com.typesafe.config.ConfigFactory
+
+import java.nio.file._
+import org.slf4j.LoggerFactory
+
+import scala.collection.immutable.Queue
+
+case class Address(host: String, port: Int)
+case class SbtConfig(config: String)
+case class InputsWithIp(inputs: Inputs, ip: String)
+
+
+case class GetPaste(id: Int)
 case object LoadBalancerStateRequest
 case class LoadBalancerStateResponse(loadBalancer: LoadBalancer[String, ActorSelection])
 
 
-// import api._
+class DispatchActor(progressActor: ActorRef) extends Actor {
 
-// import scala.collection.JavaConverters._
+  private val configuration = ConfigFactory.load().getConfig("com.olegych.scastie.balancer")
 
-// import akka.actor.{Actor, ActorRef, ActorSelection}
-// import akka.remote.DisassociatedEvent
-// import akka.routing.{ActorSelectionRoutee, RoundRobinRoutingLogic, Router}
+  private val portsStart = configuration.getInt("remote-ports-start")
+  private val portsSize = configuration.getInt("remote-ports-size")
+  private val host = configuration.getString("remote-hostname")
 
-// import java.nio.file._
-// import org.slf4j.LoggerFactory
+  private val ports = (0 until portsSize).map(portsStart + _)
 
-// import scala.collection.mutable.{Map => MMap}
+  private var remoteSelections = ports.map{port =>
+    val selection = context.actorSelection(
+      s"akka.tcp://SbtRemote@$host:$port/user/SbtActor"
+    )
+    (host, port) -> selection
+  }.toMap
 
-// import scala.collection.immutable.Queue
+  private var loadBalancer: LoadBalancer[String, ActorSelection] = {
+    val servers = remoteSelections.to[Vector].map{ case (_, ref) =>
+      Server(ref, Inputs.default.sbtConfig)
+    }
 
-// case class Address(host: String, port: Int)
-// case class SbtConfig(config: String)
-// case class InputsWithIp(inputs: Inputs, ip: String)
+    val history = History(Queue.empty[Record[String]], size = 100)
+    LoadBalancer(servers, history)
+  }
 
+  private val log = LoggerFactory.getLogger(getClass)
 
-// case class GetPaste(id: Int)
+  override def preStart = {
+    context.system.eventStream.subscribe(self, classOf[DisassociatedEvent])
+    ()
+  }
 
-// class DispatchActor(progressActor: ActorRef) extends Actor {
+  private val container = new PastesContainer(
+    Paths.get(configuration.getString("pastes-dir"))
+  )
 
-//   private val portsStart = configuration.getInt("sbt-remote-ports-start").get
-//   private val portsSize = configuration.getInt("sbt-remote-ports-size").get
-//   private val host = configuration.getString("sbt-remote-hostname").get
+  private val portsInfo = ports.mkString("[", ", ", "]")
+  log.info(s"connecting to: $host $portsInfo")
 
-//   private val ports = (0 until portsSize).map(portsStart + _)
+  def receive = {
+    case format: FormatRequest => {
+      val server = loadBalancer.getRandomServer
+      server.ref.tell(format, sender)
+      ()
+    }
 
-//   private var remoteSelections = ports.map{port =>
-//     val selection = context.actorSelection(
-//       s"akka.tcp://SbtRemote@$host:$port/user/SbtActor"
-//     )
-//     (host, port) -> selection
-//   }.toMap
+    case InputsWithIp(inputs, ip) => {
+      val id = container.writePaste(inputs)
 
-//   private var loadBalancer: LoadBalancer[String, ActorSelection] = {
-//     val servers = remoteSelections.to[Vector].map{ case (_, ref) =>
-//       Server(ref, Inputs.default.sbtConfig)
-//     }
+      val (server, balancer) = loadBalancer.add(Task(inputs.sbtConfig, Ip(ip), id))
+      loadBalancer = balancer
+      server.ref.tell(SbtTask(id, inputs, ip, progressActor), self)
+      sender ! Ressource(id)
+    }
 
-//     val history = History(Queue.empty[Record[String]], size = 100)
-//     LoadBalancer(servers, history)
-//   }
+    case GetPaste(id) => {
+      sender !
+        container.readPaste(id).zip(container.readOutput(id)).headOption.map {
+          case (inputs, progresses) =>
+            FetchResult(inputs, progresses)
+        }
+    }
 
-//   private val log = LoggerFactory.getLogger(getClass)
+    case progress: api.PasteProgress => {
+      if(progress.done) {
+        loadBalancer = loadBalancer.done(progress.id)
+      }
+      container.appendOutput(progress)
+    }
 
-//   override def preStart = {
-//     context.system.eventStream.subscribe(self, classOf[DisassociatedEvent])
-//     ()
-//   }
+    case event: DisassociatedEvent => {
+      for {
+        host <- event.remoteAddress.host
+        port <- event.remoteAddress.port
+        ref  <- remoteSelections.get((host, port))
+      } {
+        log.warn(event.toString)
+        log.warn("removing disconnected: " + ref)
+        remoteSelections = remoteSelections - ((host, port))
+        loadBalancer = loadBalancer.removeServer(ref)
+      }
+    }
 
-//   private val container = new PastesContainer(
-//     Paths.get(configuration.getString("pastes.data.dir").get)
-//   )
-
-//   private val portsInfo = ports.mkString("[", ", ", "]")
-//   log.info(s"connecting to: $host $portsInfo")
-
-//   def receive = {
-//     case format: FormatRequest => {
-//       val server = loadBalancer.getRandomServer
-//       server.ref.tell(format, sender)
-//       ()
-//     }
-
-//     case InputsWithIp(inputs, ip) => {
-//       val id = container.writePaste(inputs)
-
-//       val (server, balancer) = loadBalancer.add(Task(inputs.sbtConfig, Ip(ip), id))
-//       loadBalancer = balancer
-//       server.ref.tell(SbtTask(id, inputs, ip, progressActor), self)
-//       sender ! Ressource(id)
-//     }
-
-//     case GetPaste(id) => {
-//       sender !
-//         container.readPaste(id).zip(container.readOutput(id)).headOption.map {
-//           case (inputs, progresses) =>
-//             FetchResult(inputs, progresses)
-//         }
-//     }
-
-//     case progress: api.PasteProgress => {
-//       if(progress.done) {
-//         loadBalancer = loadBalancer.done(progress.id)
-//       }
-//       container.appendOutput(progress)
-//     }
-
-//     case event: DisassociatedEvent => {
-//       for {
-//         host <- event.remoteAddress.host
-//         port <- event.remoteAddress.port
-//         ref  <- remoteSelections.get((host, port))
-//       } {
-//         log.warn(event.toString)
-//         log.warn("removing disconnected: " + ref)
-//         remoteSelections = remoteSelections - ((host, port))
-//         loadBalancer = loadBalancer.removeServer(ref)
-//       }
-//     }
-
-//     case LoadBalancerStateRequest => {
-//       sender ! LoadBalancerStateResponse(loadBalancer)
-//     }
-//   }
-// }
+    case LoadBalancerStateRequest => {
+      sender ! LoadBalancerStateResponse(loadBalancer)
+    }
+  }
+}
