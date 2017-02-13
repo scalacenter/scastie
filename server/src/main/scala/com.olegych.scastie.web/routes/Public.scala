9ed56bc61b39522c0ee307ce8b2e6e4cfd4383f7
Guillaume Massé <masgui@gmail.com>
2017-02-13 19:21:18 +0100

    sse demo

--- server/src/main/scala/com.olegych.scastie.web/routes/Public.scala
+++ server/src/main/scala/com.olegych.scastie.web/routes/Public.scala
@@ -5,6 +5,16 @@ package routes
 import TwirlSupport._
 import akka.http.scaladsl.server.Directives._
 
+import akka.NotUsed
+import akka.stream.scaladsl.Source
+import de.heikoseeberger.akkasse.ServerSentEvent
+import de.heikoseeberger.akkasse.EventStreamMarshalling._
+
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import scala.concurrent.duration.DurationInt
+
+
 object Public {
   val routes =
     concat(
@@ -13,6 +23,26 @@ object Public {
           complete(views.html.beta())
         )
       ),
+      get(
+        path("time-demo")(
+          complete(views.html.timeDemo())
+        )
+      ),
+      get(
+        path("time")(
+          complete(
+            Source
+              .tick(2.seconds, 2.seconds, NotUsed)
+              .map(_ => LocalTime.now())
+              .map(timeToServerSentEvent)
+              .keepAlive(1.second, () => ServerSentEvent.heartbeat)
+          )
+        )
+      ),
       Assets.routes
     )
+
+
+  private def timeToServerSentEvent(time: LocalTime) =
+    ServerSentEvent(DateTimeFormatter.ISO_LOCAL_TIME.format(time))
 }
