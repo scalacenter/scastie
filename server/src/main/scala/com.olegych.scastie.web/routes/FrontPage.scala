d588939a6a03636f18816c75858ed3b9288cb9a9
Guillaume Massé <masgui@gmail.com>
2017-02-13 16:53:49 +0100

    login with github

--- server/src/main/scala/com.olegych.scastie.web/routes/FrontPage.scala
+++ server/src/main/scala/com.olegych.scastie.web/routes/FrontPage.scala
@@ -2,34 +2,14 @@ package com.olegych.scastie
 package web
 package routes
 
-import oauth2._
 import views._
 import TwirlSupport._
 
 import akka.http.scaladsl._
+
 import server.Directives._
 
-// import com.softwaremill.session._
-// import SessionDirectives._
-// import SessionOptions._
-
-class FrontPage(session: GithubUserSession, production: Boolean) {
-  // import session._
-
-  // def index =
-  //   optionalSession(refreshable, usingCookies)(userId =>
-  //     // session.getUser(userId).map(_.user)
-  //     complete(html.index())
-  //   )
-
-  // def requireLogin[T](f: => T): T =
-  //   optionalSession(refreshable, usingCookies)(userId =>
-  //     session.getUser(userId).map(_.user) match {
-  //       case Some(user) => complete(f)
-  //       case None => redirect("/beta")
-  //     }
-  //   )
-
+class FrontPage(production: Boolean) {
   val routes = (
     get(
       concat(
