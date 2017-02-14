e6cf1eebfe4fa8a2e9e009de3f04f96252529f3a
Guillaume Massé <masgui@gmail.com>
2017-02-14 11:18:08 +0100

    fix assets not serving on clean build

--- server/src/main/scala/com.olegych.scastie.web/routes/Assets.scala
+++ server/src/main/scala/com.olegych.scastie.web/routes/Assets.scala
@@ -11,25 +11,7 @@ object Assets {
         path("assets" / "lib" / Remaining)(path ⇒
           getFromResource("lib/" + path)),
         path("assets" / "public" / Remaining)(path ⇒
-          getFromResource("public/" + path)),
-        path("assets" / "client-opt.js")(
-          getFromResource("client-opt.js")
-        ),
-        path("assets" / "main.css")(
-          getFromResource("main.css")
-        ),
-        path("assets" / "client-fastopt.js")(
-          getFromResource("client-fastopt.js")
-        ),
-        path("assets" / "client-opt.js.map")(
-          getFromResource("client-opt.js.map")
-        ),
-        path("assets" / "client-fastopt.js.map")(
-          getFromResource("client-fastopt.js.map")
-        ),
-        path("assets" / "client-jsdeps.js")(
-          getFromResource("client-jsdeps.js")
-        )
+          getFromResource("public/" + path))
       )
     )
 }
