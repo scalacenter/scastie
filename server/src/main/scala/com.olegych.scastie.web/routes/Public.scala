0cf4573096aa4539712415c0abec31e2eed2cd27
Guillaume Massé <masgui@gmail.com>
2017-02-14 15:31:33 +0100

    sse & websocket demo to debug proxy issues

--- server/src/main/scala/com.olegych.scastie.web/routes/Public.scala
+++ server/src/main/scala/com.olegych.scastie.web/routes/Public.scala
@@ -6,7 +6,12 @@ import TwirlSupport._
 import akka.http.scaladsl.server.Directives._
 
 import akka.NotUsed
-import akka.stream.scaladsl.Source
+import akka.http.scaladsl._
+import model._
+import ws.TextMessage._
+import server.Directives._
+import akka.stream.scaladsl._
+
 import de.heikoseeberger.akkasse.ServerSentEvent
 import de.heikoseeberger.akkasse.EventStreamMarshalling._
 
@@ -14,6 +19,7 @@ import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import scala.concurrent.duration.DurationInt
 
+import scala.concurrent.Future
 
 object Public {
   val routes =
@@ -24,25 +30,44 @@ object Public {
         )
       ),
       get(
-        path("time-demo")(
-          complete(views.html.timeDemo())
-        )
-      ),
-      get(
-        path("time")(
-          complete(
-            Source
-              .tick(0.second, 1.seconds, NotUsed)
-              .take(5)
-              .map(_ => LocalTime.now())
-              .map(timeToServerSentEvent)
+        concat(
+          path("sse-demo")(
+            complete(views.html.sseDemo())
+          ),
+          path("websocket-demo")(
+            complete(views.html.websocketDemo())
+          ),
+          path("demo-sse-progress" / Segment)(progressId =>
+            complete(
+              source(progressId).map(time => ServerSentEvent(time))
+            )
+          ),
+          path("demo-websocket-progress" / Segment)(progressId =>
+            handleWebSocketMessages(
+              webSocket(progressId)
+            )
           )
         )
       ),
       Assets.routes
     )
 
+  private def source(id: String) = {
+    Source
+      .tick(0.second, 1.seconds, NotUsed)
+      .take(5)
+      .map(_ => id + " == " + DateTimeFormatter.ISO_LOCAL_TIME.format(LocalTime.now()))
+  }
 
-  private def timeToServerSentEvent(time: LocalTime) =
-    ServerSentEvent(DateTimeFormatter.ISO_LOCAL_TIME.format(time))
+  private def webSocket(id: String): Flow[ws.Message, ws.Message , _] = {
+    Flow[ws.Message]
+      .mapAsync(1){
+        case Strict(c) ⇒ Future.successful(c)
+        case e => Future.failed(new Exception(e.toString))
+      }
+      .via(
+        Flow.fromSinkAndSource(Flow[String].to(Sink.ignore), source(id))
+      )
+      .map(time ⇒ ws.TextMessage.Strict(time))
+  }
 }
