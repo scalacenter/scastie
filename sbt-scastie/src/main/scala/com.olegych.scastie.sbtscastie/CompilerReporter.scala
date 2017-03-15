3342f15bae3b7090b546c12d43188991b0a841fd
Guillaume Massé <masgui@gmail.com>
2017-03-15 17:15:40 -0400

    scalafmt

--- sbt-scastie/src/main/scala/com.olegych.scastie.sbtscastie/CompilerReporter.scala
+++ sbt-scastie/src/main/scala/com.olegych.scastie.sbtscastie/CompilerReporter.scala
@@ -19,48 +19,50 @@ object CompilerReporter {
   )
 
   val setting: sbt.Def.Setting[_] =
-    compilerReporter in (Compile, compile) := Some(new xsbti.Reporter {
-      private val buffer = collection.mutable.ArrayBuffer.empty[Problem]
-      def reset(): Unit = buffer.clear()
-      def hasErrors: Boolean = buffer.exists(_.severity == Severity.Error)
-      def hasWarnings: Boolean = buffer.exists(_.severity == Severity.Warn)
+    compilerReporter in (Compile, compile) := Some(
+      new xsbti.Reporter {
+        private val buffer = collection.mutable.ArrayBuffer.empty[Problem]
+        def reset(): Unit = buffer.clear()
+        def hasErrors: Boolean = buffer.exists(_.severity == Severity.Error)
+        def hasWarnings: Boolean = buffer.exists(_.severity == Severity.Warn)
 
-      def annoying(in: Problem): Boolean = {
-        in.severity == xsbti.Severity.Warn &&
-        in.message == "a pure expression does nothing in statement position; you may be omitting necessary parentheses"
-      }
+        def annoying(in: Problem): Boolean = {
+          in.severity == xsbti.Severity.Warn &&
+          in.message == "a pure expression does nothing in statement position; you may be omitting necessary parentheses"
+        }
 
-      def printSummary(): Unit = {
-        def toApi(p: Problem): api.Problem = {
-          def toOption[T](m: Maybe[T]): Option[T] = {
-            if (m.isEmpty) None
-            else Some(m.get)
-          }
-          val severity =
-            p.severity match {
-              case xsbti.Severity.Info => api.Info
-              case xsbti.Severity.Warn => api.Warning
-              case xsbti.Severity.Error => api.Error
+        def printSummary(): Unit = {
+          def toApi(p: Problem): api.Problem = {
+            def toOption[T](m: Maybe[T]): Option[T] = {
+              if (m.isEmpty) None
+              else Some(m.get)
             }
-          api.Problem(severity,
-                      toOption(p.position.line).map(_.toInt),
-                      p.message)
+            val severity =
+              p.severity match {
+                case xsbti.Severity.Info => api.Info
+                case xsbti.Severity.Warn => api.Warning
+                case xsbti.Severity.Error => api.Error
+              }
+            api.Problem(severity,
+                        toOption(p.position.line).map(_.toInt),
+                        p.message)
+          }
+          if (problems.nonEmpty) {
+            println(uwrite(problems.filterNot(annoying).map(toApi)))
+          }
         }
-        if (problems.nonEmpty) {
-          println(uwrite(problems.filterNot(annoying).map(toApi)))
+        def problems: Array[Problem] = buffer.toArray
+        def log(pos: Position, msg: String, sev: Severity): Unit = {
+          object MyProblem extends Problem {
+            def category: String = "foo"
+            def severity: Severity = sev
+            def message: String = msg
+            def position: Position = pos
+            override def toString = s"$position:$severity: $message"
+          }
+          buffer.append(MyProblem)
         }
+        def comment(pos: xsbti.Position, msg: String): Unit = ()
       }
-      def problems: Array[Problem] = buffer.toArray
-      def log(pos: Position, msg: String, sev: Severity): Unit = {
-        object MyProblem extends Problem {
-          def category: String = "foo"
-          def severity: Severity = sev
-          def message: String = msg
-          def position: Position = pos
-          override def toString = s"$position:$severity: $message"
-        }
-        buffer.append(MyProblem)
-      }
-      def comment(pos: xsbti.Position, msg: String): Unit = ()
-    })
+    )
 }
