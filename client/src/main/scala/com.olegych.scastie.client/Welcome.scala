a3a1ad8bfdf5c2173f3f802318a85beed1db4f9c
dominv <domingo.v@47deg.com>
2017-03-24 11:12:14 +0100

    Improving Modal boxes styles

--- client/src/main/scala/com.olegych.scastie.client/Welcome.scala
+++ client/src/main/scala/com.olegych.scastie.client/Welcome.scala
@@ -18,26 +18,30 @@ object Welcome {
         div(`class` := "modal", displayWelcome)(
           input(`type` := "checkbox", `id` := "modal-welcome", `class` := "modal-state"),
           div(`class` := "modal-fade-screen")(
-            div(`class` := "modal-inner")(
-              div(`for` := "modal-welcome", `class` := "modal-close", onClick ==> backend.toggleWelcome))(
-              h1("Welcome to Scastie!"),
-              p(`class` := "modal-intro","Scastie is an interactive playground for Scala."),
-              h2("Run / Edit"),
-              p(`class` := "modal-intro","The code editor where you can write and run your code."),
-              h2("Build Settings"),
-              p(`class` := "modal-intro","""In Build Settings you can change the Scala version and add libraries,
-                            choose your desired target and even add your own custom sbt configuration."""),
-              h2("User's Code Snippets"),
-              p(`class` := "modal-intro","Your saved code fragments will appear here and you’ll be able to edit or share them."),
-              h2("Console"),
-              p(`class` := "modal-intro","You can see your code’s output in the Scastie’s console."),
-              h2("Feedback"),
-              p(`class` := "modal-intro","You can join our Gitter channel and send issues."),
-              p(`class` := "modal-intro")(
-                i(`class` := "fa fa-question-circle"),
-                "If you want to learn more about how Scastie works, you can go to our ",
-                a(href := "","Help", onClick ==> backend.toggleWelcomeHelp),
-                "."
+            div(`class` := "modal-window")(
+              div(`class` := "modal-header")(
+                div(`for` := "modal-welcome", `class` := "modal-close", onClick ==> backend.toggleWelcome))(
+                h1("Welcome to Scastie!")
+              ),
+              div(`class` := "modal-inner")(
+                p(`class` := "modal-intro","Scastie is an interactive playground for Scala."),
+                h2("Run / Edit"),
+                p(`class` := "modal-intro","The code editor where you can write and run your code."),
+                h2("Build Settings"),
+                p(`class` := "modal-intro","""In Build Settings you can change the Scala version and add libraries,
+                              choose your desired target and even add your own custom sbt configuration."""),
+                h2("User's Code Snippets"),
+                p(`class` := "modal-intro","Your saved code fragments will appear here and you’ll be able to edit or share them."),
+                h2("Console"),
+                p(`class` := "modal-intro","You can see your code’s output in the Scastie’s console."),
+                h2("Feedback"),
+                p(`class` := "modal-intro","You can join our Gitter channel and send issues."),
+                p(`class` := "modal-intro")(
+                  i(`class` := "fa fa-question-circle"),
+                  "If you want to learn more about how Scastie works, you can go to our ",
+                  a(href := "","Help", onClick ==> backend.toggleWelcomeHelp),
+                  "."
+                )
               )
             )
           )
