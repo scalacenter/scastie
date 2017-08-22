ed862062dd64f266fea39bdb9eef2df6e447ca7b
Guillaume Massé <masgui@gmail.com>
2017-08-22 12:32:29 +0200

    Style for embedded Scastie

--- client/src/main/resources/app.js
+++ client/src/main/resources/app.js
@@ -18,7 +18,34 @@ if (window.location.hostname != "localhost") {
 
 const Main = scalajs.scastie.ClientMain;
 
-Main.embedded("pre");
+if (window.location.search === "") {
+  Main.main();
+} else {
+  var h1 = document.createElement("h1");
+  h1.innerText = "Foo Bar Tutorial";
+
+  var pre = document.createElement("pre");
+  pre.innerText = "1+1";
+
+  var p1 = document.createElement("p");
+  p1.innerText = "some text p1";
+  
+  var pre2 = document.createElement("pre");
+  pre2.innerText = "List(1, 2, 3)";
+
+  var p2 = document.createElement("p");
+  p2.innerText = "end of document";
+
+  document.body.appendChild(h1);
+  document.body.appendChild(pre);
+  document.body.appendChild(p1);
+  document.body.appendChild(pre2);
+  document.body.appendChild(p2);
+
+  document.body.className = "scastie-embedded-dev"
+
+  Main.embedded("pre");
+}
 
 module.exports = {
   com: {
