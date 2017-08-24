31e971d4335e0df1ab720953e0868e1b1d03efcc
Guillaume Massé <masgui@gmail.com>
2017-08-24 17:58:26 +0200

    Set all inputs from embedded mode
    Set a specific default code for Dotty fix #234

--- api/src/main/scala/com.olegych.scastie.api/ScalaTarget.scala
+++ api/src/main/scala/com.olegych.scastie.api/ScalaTarget.scala
@@ -89,72 +89,6 @@ object ScalaTarget {
     }
   }
 
-  val allVersions = List(
-    "2.13.0-M1",
-    "2.12.3",
-    "2.12.1",
-    "2.12.0",
-    "2.12.0-RC2",
-    "2.12.0-RC1",
-    "2.12.0-M5",
-    "2.12.0-M4",
-    "2.12.0-M3",
-    "2.12.0-M2",
-    "2.12.0-M1",
-    "2.11.11",
-    "2.11.8",
-    "2.11.6",
-    "2.11.5",
-    "2.11.4",
-    "2.11.3",
-    "2.11.2",
-    "2.11.1",
-    "2.11.0",
-    "2.11.0-RC4",
-    "2.11.0-RC3",
-    "2.11.0-RC1",
-    "2.11.0-M8",
-    "2.11.0-M7",
-    "2.11.0-M6",
-    "2.11.0-M5",
-    "2.11.0-M3",
-    "2.10.6",
-    "2.10.5",
-    "2.10.4-RC3",
-    "2.10.4-RC2",
-    "2.10.3",
-    "2.10.3-RC3",
-    "2.10.3-RC2",
-    "2.10.3-RC1",
-    "2.10.2",
-    "2.10.2-RC2",
-    "2.10.2-RC1",
-    "2.10.1",
-    "2.10.1-RC3",
-    "2.10.1-RC1",
-    "2.10.0",
-    "2.10.0-RC5",
-    "2.10.0-RC4",
-    "2.10.0-RC3",
-    "2.10.0-RC2",
-    "2.10.0-RC1",
-    "2.10.0-M7",
-    "2.10.0-M6",
-    "2.10.0-M5",
-    "2.10.0-M4",
-    "2.10.0-M2",
-    "2.10.0-M1",
-    "2.9.3",
-    "2.9.3-RC2",
-    "2.9.3-RC1",
-    "2.9.2",
-    "2.9.2-RC3",
-    "2.9.2-RC2",
-    "2.9.1-1-RC1",
-    "2.9.1-1",
-    "2.9.0"
-  )
-
   private def runtimeDependencyFrom(
       target: ScalaTarget
   ): Option[ScalaDependency] = {
@@ -173,7 +107,6 @@ object ScalaTarget {
   }
 
   case class Jvm(scalaVersion: String) extends ScalaTarget {
-
     def hasEnsimeSupport: Boolean = {
       scalaVersion.startsWith("2.12") ||
       scalaVersion.startsWith("2.11")
@@ -328,6 +261,31 @@ object ScalaTarget {
 
   object Dotty {
     def default = Dotty(defaultDottyVersion)
+
+    def defaultCode = 
+      """|// You can find more examples here:
+         |//   https://github.com/lampepfl/dotty-example-project
+         |
+         |abstract class Base {
+         |  def message: String
+         |}
+         |
+         |class A extends Base {
+         |  def message: String = "Hello"
+         |}
+         |
+         |class B extends Base {
+         |  def message: String = "Dotty!"
+         |}
+         |
+         |object Main {
+         |  def main(args: Array[String]): Unit = { 
+         |    def helloDotty(msgs: (A | B)*): String = 
+         |      msgs.map(_.message).mkString(", ")
+         |    
+         |    println(helloDotty(new A, new B))
+         |  }
+         |}""".stripMargin
   }
 
   case class Dotty(dottyVersion: String) extends ScalaTarget {
