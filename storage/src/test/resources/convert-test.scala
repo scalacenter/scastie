639e8e02c5b09517289107627977ec175fc095f9
Guillaume Massé <masgui@gmail.com>
2017-08-25 15:08:40 +0200

    all test passes

--- storage/src/test/resources/convert-test.scala
+++ storage/src/test/resources/convert-test.scala
@@ -18,12 +18,16 @@ resolvers += Resolver.sonatypeRepo("releases")
 addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.0")
 
 // if your project uses multiple Scala versions, use this for cross building
-addCompilerPlugin("org.spire-math" % "kind-projector" % "0.9.0" cross CrossVersion.binary)
+addCompilerPlugin(
+  "org.spire-math" % "kind-projector" % "0.9.0" cross CrossVersion.binary
+)
 
 // if your project uses both 2.10 and polymorphic lambdas
 libraryDependencies ++= (scalaBinaryVersion.value match {
   case "2.10" =>
-    compilerPlugin("org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full) :: Nil
+    compilerPlugin(
+      "org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full
+    ) :: Nil
   case _ =>
     Nil
 })
