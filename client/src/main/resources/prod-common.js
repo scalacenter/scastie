5c31a7b8e4c0d47c57de31b4a13612e9ca5ca6ed
Guillaume Massé <masgui@gmail.com>
2017-09-20 14:01:40 +0200

    Client: Fix prod embedded entry point

--- client/src/main/resources/prod-common.js
+++ client/src/main/resources/prod-common.js
@@ -1,9 +1,9 @@
 var clientOpt = require("scalajs/client-opt.js");
 var defaultServerUrl = "https://scastie.scala-lang.org";
-var embeddedStylesheetUrl = "/public/embedded.css"
+var embeddedStylesheetUrl = "/public/embedded.css";
 
 module.exports = {
-  defaultServerUrl: defaultServerUrl,  
+  defaultServerUrl: defaultServerUrl,
   clientOpt: clientOpt,
   scastie: {
     ScastieMain: new clientOpt.scastie.ScastieMain(defaultServerUrl),
