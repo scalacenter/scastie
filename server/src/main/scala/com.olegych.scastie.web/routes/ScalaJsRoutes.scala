668609d24c741a98f785b2d1790d0fcab8a87377
Guillaume Massé <masgui@gmail.com>
2017-09-25 10:54:07 +0200

    Server: gzip Scala.js user output

--- server/src/main/scala/com.olegych.scastie.web/routes/ScalaJsRoutes.scala
+++ server/src/main/scala/com.olegych.scastie.web/routes/ScalaJsRoutes.scala
@@ -8,6 +8,7 @@ import akka.pattern.ask
 import akka.actor.{ActorRef, ActorSystem}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.coding.Gzip
 
 import scala.concurrent.duration.DurationInt
 
@@ -17,30 +18,32 @@ class ScalaJsRoutes(dispatchActor: ActorRef)(implicit system: ActorSystem) {
   implicit val timeout: Timeout = Timeout(1.seconds)
 
   val routes: Route =
-    concat(
-      snippetIdEnd(Shared.scalaJsHttpPathPrefix, ScalaTarget.Js.targetFilename)(
-        sid =>
-          complete(
-            (dispatchActor ? FetchScalaJs(sid))
-              .mapTo[Option[FetchResultScalaJs]]
-              .map(_.map(_.content))
-        )
-      ),
-      snippetIdEnd(Shared.scalaJsHttpPathPrefix, ScalaTarget.Js.sourceFilename)(
-        sid =>
-          complete(
-            (dispatchActor ? FetchScalaSource(sid))
-              .mapTo[Option[FetchResultScalaSource]]
-              .map(_.map(_.content))
-        )
-      ),
-      snippetIdEnd(Shared.scalaJsHttpPathPrefix,
-                   ScalaTarget.Js.sourceMapFilename)(
-        sid =>
-          complete(
-            (dispatchActor ? FetchScalaJsSourceMap(sid))
-              .mapTo[Option[FetchResultScalaJsSourceMap]]
-              .map(_.map(_.content))
+    encodeResponseWith(Gzip)(
+      concat(
+        snippetIdEnd(Shared.scalaJsHttpPathPrefix, ScalaTarget.Js.targetFilename)(
+          sid =>
+            complete(
+              (dispatchActor ? FetchScalaJs(sid))
+                .mapTo[Option[FetchResultScalaJs]]
+                .map(_.map(_.content))
+          )
+        ),
+        snippetIdEnd(Shared.scalaJsHttpPathPrefix, ScalaTarget.Js.sourceFilename)(
+          sid =>
+            complete(
+              (dispatchActor ? FetchScalaSource(sid))
+                .mapTo[Option[FetchResultScalaSource]]
+                .map(_.map(_.content))
+          )
+        ),
+        snippetIdEnd(Shared.scalaJsHttpPathPrefix,
+                     ScalaTarget.Js.sourceMapFilename)(
+          sid =>
+            complete(
+              (dispatchActor ? FetchScalaJsSourceMap(sid))
+                .mapTo[Option[FetchResultScalaJsSourceMap]]
+                .map(_.map(_.content))
+          )
         )
       )
     )
