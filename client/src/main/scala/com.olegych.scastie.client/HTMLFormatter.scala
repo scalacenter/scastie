c2ad8c64f7f984ff5a7a7962a6bd328c362ab5cc
Abdelhakim Mouslih <mouslih.abdelhakim@gmail.com>
2018-02-01 23:48:14 +0100

    Escape HTML in the console's output (fix #362)

---
+++ client/src/main/scala/com.olegych.scastie.client/HTMLFormatter.scala
@@ -0,0 +1,22 @@
+package com.olegych.scastie.client
+
+object HTMLFormatter {
+  private val escapeMap =
+    Map('&' -> "&amp;", '"' -> "&quot;", '<' -> "&lt;", '>' -> "&gt;")
+
+  private def escape(text: String): String =
+    text.iterator
+      .foldLeft(new StringBuilder()) { (s, c) =>
+        escapeMap.get(c) match {
+          case Some(str)                             => s ++= str
+          case _ if c >= ' ' || "\n\r\t".contains(c) => s += c
+          case _                                     => s // noop
+        }
+      }
+      .toString
+
+  def format(notEscapedAndUnformatted: String) = {
+    val escaped = escape(notEscapedAndUnformatted)
+    AnsiColorFormatter.formatToHtml(escaped)
+  }
+}
