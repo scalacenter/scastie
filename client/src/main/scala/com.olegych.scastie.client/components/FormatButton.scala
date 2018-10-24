fc39c3ff0f8595d23c995f70f6dc1999b302e851
OlegYch <olegych@tut.by>
2018-10-24 18:26:38 +0300

    don't disable formatting

--- client/src/main/scala/com.olegych.scastie.client/components/FormatButton.scala
+++ client/src/main/scala/com.olegych.scastie.client/components/FormatButton.scala
@@ -1,6 +1,8 @@
 package com.olegych.scastie.client.components
 
-import japgolly.scalajs.react._, vdom.all._, extra._
+import japgolly.scalajs.react._
+import japgolly.scalajs.react.extra._
+import japgolly.scalajs.react.vdom.all._
 
 final case class FormatButton(inputsHasChanged: Boolean, isStatusOk: Boolean, formatCode: Reusable[Callback]) {
   @inline def render: VdomElement = FormatButton.component(this)
@@ -11,9 +13,7 @@ object FormatButton {
     Reusability.caseClass[FormatButton]
 
   private def render(props: FormatButton): VdomElement = {
-
-    val isDisabled =
-      !props.inputsHasChanged || !props.isStatusOk
+    val isDisabled = false
 
     val formatCode =
       if (!isDisabled) props.formatCode
