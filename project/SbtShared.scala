1e930d6290d0d50a9c061697addfed0a8cfd274b
OlegYch <olegych@tut.by>
2021-07-12 16:01:20 +0300

    update scala3, closes #509, closes #510

--- project/SbtShared.scala
+++ project/SbtShared.scala
@@ -22,8 +22,8 @@ object SbtShared {
     val latest211 = "2.11.12"
     val latest212 = "2.12.13"
     val latest213 = "2.13.6"
-    val stable3   = "3.0.0"
-    val latest3   = "3.0.1-RC1"
+    val stable3   = "3.0.1"
+    val latest3   = "3.0.1"
     val js = latest213
     val sbt = latest212
     val jvm = latest213
