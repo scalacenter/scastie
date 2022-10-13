996f612cecbf59b4047437978d6d188a8ced58c7
Jędrzej Rochala <48657087+rochala@users.noreply.github.com>
2022-10-13 13:02:53 +0200

    disable sentry performance (#649)

--- client/src/main/resources/prod/main.js
+++ client/src/main/resources/prod/main.js
@@ -6,7 +6,6 @@ import { BrowserTracing } from "@sentry/tracing";
 
 Sentry.init({
   dsn: "https://729713d6e2f243a4ae0b16c770e6c071@o1427772.ingest.sentry.io/6778768",
-  integrations: [new BrowserTracing()],
 
   tracesSampleRate: 1.0,
 });
