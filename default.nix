a426cc4aae5f2d98d50b7b47640969f2335e3683
Jędrzej Rochala <48657087+rochala@users.noreply.github.com>
2023-02-06 19:30:20 +0100

    Fix pipe in SSH (#757)
    * fix ssh pipe

--- default.nix
+++ default.nix
@@ -1,18 +1,18 @@
-with import (builtins.fetchGit {
-  name = "nixos-unstable-2021-11-15";
-  url = "https://github.com/nixos/nixpkgs/";
-  ref = "refs/heads/nixos-unstable";
-  rev = "931ab058daa7e4cd539533963f95e2bb0dbd41e6";
-}) {};
-  
+with (import <nixpkgs> {});
+
 stdenv.mkDerivation {
   name = "scastie";
 
-  buildInputs = with pkgs; [ sbt nodejs yarn bash ];
-
-  SBT_OPTS = "";
-
-  shellHook = ''
-    alias cls=clear
-  '';
- }
+  buildInputs =
+    let
+      sbtWithJRE = pkgs.sbt.override {
+        jre = pkgs.jdk17_headless;
+      };
+    in
+      [
+        pkgs.nodejs
+        pkgs.yarn
+        pkgs.bash
+        sbtWithJRE
+      ];
+}
