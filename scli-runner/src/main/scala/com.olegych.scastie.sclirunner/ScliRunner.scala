ff09d9f9d99c6f6a4dcdc3f09b264d3cfb35f789
Maeeen <marwanazuz@gmail.com>
2023-02-23 01:17:05 +0100

    Bsp incomprehension

--- scli-runner/src/main/scala/com.olegych.scastie.sclirunner/ScliRunner.scala
+++ scli-runner/src/main/scala/com.olegych.scastie.sclirunner/ScliRunner.scala
@@ -17,6 +17,19 @@ import com.olegych.scastie.instrumentation.InstrumentationFailureReport
 import com.olegych.scastie.instrumentation.InstrumentedInputs
 import java.io.OutputStream
 import java.io.InputStream
+import ch.epfl.scala.bsp4j.BuildClient
+import ch.epfl.scala.bsp4j.LogMessageParams
+import ch.epfl.scala.bsp4j.PublishDiagnosticsParams
+import ch.epfl.scala.bsp4j.ShowMessageParams
+import ch.epfl.scala.bsp4j.DidChangeBuildTarget
+import ch.epfl.scala.bsp4j.TaskFinishParams
+import ch.epfl.scala.bsp4j.TaskProgressParams
+import ch.epfl.scala.bsp4j.TaskStartParams
+import org.eclipse.lsp4j.jsonrpc.Launcher
+import ch.epfl.scala.bsp4j.BuildServer
+import ch.epfl.scala.bsp4j.InitializeBuildParams
+import ch.epfl.scala.bsp4j.BuildClientCapabilities
+import java.util.Collections
 
 object ScliRunner {
   case class ScliTask(snippetId: SnippetId, inputs: Inputs, ip: String, login: Option[String])
@@ -58,19 +71,65 @@ class ScliRunner {
   private var pStderr: Option[InputStream] = None
 
   private def startBsp : Unit = {
-    log.info("Starting Scala-CLI BSP")
+    log.info(s"Starting Scala-CLI BSP in folder ${workingDir.toAbsolutePath().normalize().toString()}")
     val processBuilder: ProcessBuilder = Process(Seq("scala-cli", "bsp", "."), workingDir.toFile())
     val io = BasicIO.standard(true)
-      .withInput(i => pStdin = Some(i))
+      .withInput(i => pStdin = Some(i)) 
       .withError(e => pStderr = Some(e))
       .withOutput(o => pStdout = Some(o))
 
-    // Just a quick look
-    processBuilder.run(io)
+    val process = processBuilder.run(io)
+
+    // TODO: really bad
+    while (pStdin.isEmpty || pStdout.isEmpty || pStderr.isEmpty) Thread.sleep(100)
+
+    // Create BSP connection
+    import java.util.concurrent._
+    val es = Executors.newFixedThreadPool(1)
+
+    val bspLauncher = new Launcher.Builder[BuildServer]()
+      .setOutput(pStdin.get)
+      .setInput(pStdout.get)
+      .setLocalService(localClient)
+      .setExecutorService(es)
+      .setRemoteInterface(classOf[BuildServer])
+      .create()
+
+    bspServer = Some(bspLauncher.getRemoteProxy())
+
+    listeningThread = Some(new Thread {
+      override def run() = bspLauncher.startListening().get()
+    })
+
+    log.info("Initializing workspace")
+    val initialize = bspServer.get.buildInitialize(new InitializeBuildParams(
+      "BspClient",
+      "1.0.0", // TODO: maybe not hard-code the version? not really much important
+      "2.1.0-M3", // TODO: same
+      workingDir.toAbsolutePath().normalize().toUri().toString(),
+      new BuildClientCapabilities(Collections.singletonList("scala"))
+    )).get // Force to wait
+    log.info("OK")
   }
 
   private def scalaDepToFullName = (dep: ScalaDependency) => s"${dep.groupId}::${dep.artifact}:${dep.version}"
 
+  // Client
+  class BspClient extends BuildClient {
+    def onBuildLogMessage(params: LogMessageParams): Unit = log.info(s"onBuildLogMessage $params")
+    def onBuildPublishDiagnostics(params: PublishDiagnosticsParams): Unit = log.info(s"onBuildPublishDiagnostics $params")
+    def onBuildShowMessage(params: ShowMessageParams): Unit = log.info(s"onBuildShowMessage $params")
+    def onBuildTargetDidChange(params: DidChangeBuildTarget): Unit = log.info(s"onBuildTargetDidChange $params")
+    def onBuildTaskFinish(params: TaskFinishParams): Unit = log.info(s"onBuildTaskFinish $params")
+    def onBuildTaskProgress(params: TaskProgressParams): Unit = log.info(s"onBuildTaskProgress $params")
+    def onBuildTaskStart(params: TaskStartParams): Unit = log.info(s"onBuildTaskStart $params")
+
+    override def onConnectWithServer(server: BuildServer): Unit = log.info("connected to server")
+  }
+  private val localClient = new BspClient()
+  private var bspServer: Option[BuildServer] = None
+  private var listeningThread: Option[Thread] = None
 
   startBsp
+
 }
