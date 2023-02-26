acbc416bee4193ca01246783faf75d6087e2c467
Maeeen <marwanazuz@gmail.com>
2023-02-27 00:28:38 +0100

    Refactoring

--- scli-runner/src/main/scala/com.olegych.scastie.sclirunner/BspClient.scala
+++ scli-runner/src/main/scala/com.olegych.scastie.sclirunner/BspClient.scala
@@ -1,21 +1,27 @@
 package com.olegych.scastie.sclirunner
 
-import java.io.InputStream
-import java.io.OutputStream
 import com.typesafe.scalalogging.Logger
-import ch.epfl.scala.bsp4j._
-import java.util.concurrent._
+import scala.collection.mutable.{Map, HashMap}
 import org.eclipse.lsp4j.jsonrpc.Launcher
-import java.nio.file.Path
+import ch.epfl.scala.bsp4j._
 import java.util.Collections
+import java.util.concurrent.Executors
+import java.util.concurrent.CompletableFuture
+import java.io.{InputStream, OutputStream}
+import java.nio.file.Path
 
 object BspClient {
-
+  case class BspClientRun(output: List[String])
+  
+  case class NoTargetsFoundException(err: String) extends Exception
+  case class NoMainClassFound(err: String) extends Exception
+  case class CompilationError(err: PublishDiagnosticsParams) extends Exception
 }
 
-trait ScalaCliServer extends BuildServer with ScalaBuildServer { }
+trait ScalaCliServer extends BuildServer with ScalaBuildServer
 
 class BspClient(private val workingDir: Path, private val inputStream: InputStream, private val outputStream: OutputStream) {
+  import BspClient._
 
   private val log = Logger("BspClient")
 
@@ -37,71 +43,89 @@ class BspClient(private val workingDir: Path, private val inputStream: InputStre
   }
   listeningThread.start()
 
+  // Runs asynchronously a build
+  def build(id: String) = {
+    val future = new CompletableFuture[BspClientRun]()
 
-  log.info("Initializing workspace")
-  private val initializeRequest = bspServer.buildInitialize(new InitializeBuildParams(
-    "BspClient",
-    "1.0.0", // TODO: maybe not hard-code the version? not really much important
-    "2.1.0-M3", // TODO: same
-    workingDir.toAbsolutePath().normalize().toUri().toString(),
-    new BuildClientCapabilities(Collections.singletonList("scala"))
-  )).get // Force to wait
+    bspServer.workspaceBuildTargets().thenAccept(buildTargets => {
+      // Finds the first "run" target
+      val target = buildTargets.getTargets().stream()
+        .filter(t => {
+          val isTestBuild = t.getTags().stream().anyMatch(tag => tag.equals("test"))
+          !isTestBuild
+        })
+        .findFirst()
 
-  log.info(s"result build/initialize: ${initializeRequest}")
-  bspServer.onBuildInitialized()
+      if (target.isEmpty()) future.completeExceptionally(NoTargetsFoundException("No build target found."))
+      else {
+        val targetId = target.get().getId()
 
+        // trigger compile
+        bspServer.buildTargetCompile({
+          val param = new CompileParams(Collections.singletonList(targetId))
+          param.setOriginId(id + "-compile")
+          param
+        }).thenAccept(compileResult => {
 
-  def build: Unit = {
-    val buildTargets = bspServer.workspaceBuildTargets().get
-    val target = buildTargets.getTargets().stream()
-      .filter(t => {
-        val isTestBuild = t.getTags().stream().anyMatch(tag => tag.equals("test"))
-        !isTestBuild
-      })
-      .findFirst()
+          if (compileResult.getStatusCode() == StatusCode.ERROR) {
+            // failed compilation
+            future.completeExceptionally(CompilationError(diagnostics(id + "-compile")))
+            diagnostics.remove(id + "-compile")
+          } else {
 
-    if (target.isEmpty()) throw new Error("No build target") // TODO: appropriate exceptions
+            // get first main class
+            bspServer.buildTargetScalaMainClasses({
+              val param = new ScalaMainClassesParams(Collections.singletonList(targetId))
+              param.setOriginId(id + "-main-classes")
+              param
+            }).thenAccept(scalaMainClasses => {
+              val classes = scalaMainClasses.getItems()
 
-    val buildTargetId = target.get().getId()
+              if (classes.size() == 0) future.completeExceptionally(NoMainClassFound("No main class found."))
+              else {
+                val mainClass = classes.get(0)
 
-    log.info("compiling")
-    val compileResult = bspServer.buildTargetCompile(
-      new CompileParams(Collections.singletonList(buildTargetId))
-    ).get
-    log.info(s"compiledResult: $compileResult")
+                // run
+                bspServer.buildTargetRun({
+                  val param = new RunParams(targetId)
+                  param.setDataKind("scala-main-class")
+                  param.setData(mainClass.getClasses().get(0))
+                  param.setOriginId(id + "-run")
+                  param
+                }).thenAccept(result => {
+                  future.complete(BspClientRun(logMessages(id + "-run").map(_.getMessage())))
+                  logMessages.remove(id + "-run")
+                })
+              }
+            })
+          }
+        })
+      }
+    })
 
-    // get first main class
-    val mainClasses = bspServer.buildTargetScalaMainClasses(new ScalaMainClassesParams(Collections.singletonList(buildTargetId))).get().getItems().get(0).getClasses().get(0)
-    // TODO: should not be a problem but check anyway that the code above does not crash ^
-    log.info(s"mainClasses: $mainClasses")    
-    
-    log.info("running")
-    val run = bspServer.buildTargetRun({
-      val param = new RunParams(buildTargetId)
-      param.setDataKind("scala-main-class")
-      param.setData(mainClasses)
-      param
-    }).get()
-    log.info(s"run: $run")
+    future
   }
 
-  // val run = bspServer.buildTargetRun()
-  //val result = run.get()
-  //println(s"result run: ${result.getStatusCode()}")
-
+  val diagnostics: Map[String, PublishDiagnosticsParams] = HashMap()
+  val logMessages: Map[String, List[LogMessageParams]] = HashMap().withDefault(_ => List())
 
   class InnerClient extends BuildClient {
-    def onBuildLogMessage(params: LogMessageParams): Unit = log.info(s"onBuildLogMessage $params")
-    def onBuildPublishDiagnostics(params: PublishDiagnosticsParams): Unit = log.info(s"onBuildPublishDiagnostics $params")
-    def onBuildShowMessage(params: ShowMessageParams): Unit = log.info(s"onBuildShowMessage $params")
-    def onBuildTargetDidChange(params: DidChangeBuildTarget): Unit = log.info(s"onBuildTargetDidChange $params")
-    def onBuildTaskFinish(params: TaskFinishParams): Unit = log.info(s"onBuildTaskFinish $params")
-    def onBuildTaskProgress(params: TaskProgressParams): Unit = log.info(s"onBuildTaskProgress $params")
-    def onBuildTaskStart(params: TaskStartParams): Unit = log.info(s"onBuildTaskStart $params")
+    def onBuildLogMessage(params: LogMessageParams): Unit = {
+      val origin = params.getOriginId()
 
-    override def onConnectWithServer(server: BuildServer): Unit = log.info("connected to server")
+      if (origin != null)
+        logMessages.put(origin, params +: logMessages(origin))
+    }
+    def onBuildPublishDiagnostics(params: PublishDiagnosticsParams): Unit = {
+      val origin = params.getOriginId()
+
+      if (origin != null)
+        diagnostics.put(origin, params)
+    }
+    def onBuildShowMessage(params: ShowMessageParams): Unit = () // log.info(s"onBuildShowMessage $params")
+    def onBuildTargetDidChange(params: DidChangeBuildTarget): Unit = () // log.info(s"onBuildTargetDidChange $params")
+    def onBuildTaskFinish(params: TaskFinishParams): Unit = () // log.info(s"onBuildTaskFinish $params")
+    def onBuildTaskProgress(params: TaskProgressParams): Unit = () // log.info(s"onBuildTaskProgress $params")
+    def onBuildTaskStart(params: TaskStartParams): Unit = () // log.info(s"onBuildTaskStart $params")
   }
-
 }
-
-
