571704b0223674759df08027d48bb1908edc95cf
rochala <rochala@tutanota.com>
2023-07-27 10:00:44 +0200

    Update binary changes

--- client/src/main/scala/com.olegych.scastie.client/components/editor/JsUtils.scala
+++ client/src/main/scala/com.olegych.scastie.client/components/editor/JsUtils.scala
@@ -1,17 +1,14 @@
 package com.olegych.scastie.client.components.editor
 
 import com.olegych.scastie.api
-import typings.codemirrorLint.codemirrorLintStrings
-
-import scalajs.js.|
+import typings.codemirrorLint.mod.Severity
 
 object JsUtils {
-  type jsSeverity = codemirrorLintStrings.error | codemirrorLintStrings.info | codemirrorLintStrings.warning
 
-  def parseSeverity(severity: api.Severity): jsSeverity = severity match {
-    case api.Error   => codemirrorLintStrings.error
-    case api.Info    => codemirrorLintStrings.info
-    case api.Warning => codemirrorLintStrings.warning
+  def parseSeverity(severity: api.Severity): Severity = severity match {
+    case api.Error   => Severity.error
+    case api.Info    => Severity.info
+    case api.Warning => Severity.warning
   }
 
 }
