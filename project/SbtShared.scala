bd401b17ab344f05c9158387e3b5f977fcf88f32
rochala <rochala@tutanota.com>
2024-01-19 16:42:45 +0100

    Bump scala version

--- project/SbtShared.scala
+++ project/SbtShared.scala
@@ -26,7 +26,7 @@ object SbtShared {
     val stableLTS  = "3.3.1"
     val stableNext = "???"
     val latestLTS  = "3.3.2-RC1"
-    val latestNext = "3.4.0-RC1"
+    val latestNext = "3.4.0-RC2"
     val js         = latest213
     val sbt        = latest212
     val jvm        = latest213
