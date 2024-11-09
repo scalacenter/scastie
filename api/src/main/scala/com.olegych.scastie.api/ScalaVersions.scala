104f900493a2761194848fbe31847d7e14f8baec
Wojciech Mazur <wmazur@virtuslab.com>
2024-11-09 15:59:19 +0100

    Update Scala 2 and Scala 3 versions

--- api/src/main/scala/com.olegych.scastie.api/ScalaVersions.scala
+++ api/src/main/scala/com.olegych.scastie.api/ScalaVersions.scala
@@ -20,13 +20,15 @@ object ScalaVersions {
           BuildInfo.stableNext,
           BuildInfo.latestLTS,
           BuildInfo.stableLTS,
+          // "3.6.0", Bad release
+          "3.5.1",
           "3.5.0",
           "3.4.3",
           "3.4.2",
           "3.4.1",
           "3.4.0",
           "3.3.3",
-          "3.3.2",
+          // "3.3.2", Bad release
           "3.3.1",
           "3.3.0",
           "3.2.2",
--- project/SbtShared.scala
+++ project/SbtShared.scala
@@ -24,9 +24,9 @@ object SbtShared {
     val latest213  = "2.13.15"
     val old3       = "3.0.2"
     val stableLTS  = "3.3.4"
-    val stableNext = "3.5.1"
+    val stableNext = "3.5.2"
     val latestLTS  = "3.3.4"
-    val latestNext = "3.5.2-RC1"
+    val latestNext = "3.6.1"
     val js         = latest213
     val sbt        = latest212
     val jvm        = latest213
