4f7dd567acc77d33f20d4a2de53a11a86a7403a1
Wojciech Mazur <wojciech.mazur95@gmail.com>
2025-06-19 14:01:23 +0200

    Add Scala 3.7.2-RC1 as latestNext

--- project/SbtShared.scala
+++ project/SbtShared.scala
@@ -26,7 +26,7 @@ object SbtShared {
     val stableLTS  = "3.3.6"
     val stableNext = "3.7.1"
     val latestLTS  = "3.3.6"
-    val latestNext = "3.7.1"
+    val latestNext = "3.7.2-RC1"
     val js         = latest213
     val sbt        = latest212
     val jvm        = latest213
