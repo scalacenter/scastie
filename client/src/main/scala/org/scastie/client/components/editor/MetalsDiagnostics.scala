7ce857d338a14166b9cf0e16f854f42686f7487e
Jakub Warchoł <93600787+warcholjakub@users.noreply.github.com>
2026-01-27 18:03:47 +0100

    fix: prevent stale diagnostics after version change

--- client/src/main/scala/org/scastie/client/components/editor/MetalsDiagnostics.scala
+++ client/src/main/scala/org/scastie/client/components/editor/MetalsDiagnostics.scala
@@ -8,30 +8,43 @@ import typings.codemirrorView.mod._
 import typings.codemirrorLint.mod._
 import scala.concurrent.duration._
 import scala.scalajs.js.timers._
+import java.util.concurrent.atomic.AtomicLong
 
 import scalajs.js
 import scalajs.concurrent.JSExecutionContext.Implicits.queue
 import scalajs.js.Thenable.Implicits._
 import js.JSConverters._
 
+object MetalsDiagnostics {
+  private val generation = new AtomicLong(0L)
+  def nextGeneration(): Long = generation.incrementAndGet()
+  def currentGeneration: Long = generation.get()
+}
+
 trait MetalsDiagnostics extends MetalsClient with DebouncingCapabilities {
-  private def debounce(fn: OnChange):  OnChange = {
+  private val myGeneration: Long = MetalsDiagnostics.nextGeneration()
+
+  private def debounce(fn: OnChange): OnChange = {
     var timeout: js.UndefOr[js.timers.SetTimeoutHandle] = js.undefined
 
     (code: String, view: EditorView) => {
       timeout.foreach(clearTimeout)
       timeout = setTimeout(1500.millis) {
-        fn(code, view)
+        if (myGeneration == MetalsDiagnostics.currentGeneration) {
+          fn(code, view)
+        }
       }
     }
   }
 
   def metalsDiags = onChangeCallback(debounce, (code, view) => ifSupported {
-    makeRequest(toLSPRequest(code, 0), "diagnostics").map(maybeText =>
-      parseMetalsResponse[Set[api.Problem]](maybeText).map { problems =>
-        val diags = problems.map(CodeEditor.problemToDiagnostic(_, view.state.doc)).toJSArray
-        view.update(js.Array(view.state.update(setDiagnostics(view.state, diags))))
-      }
-    )
+    makeRequest(toLSPRequest(code, 0), "diagnostics").map { maybeText =>
+      if (myGeneration == MetalsDiagnostics.currentGeneration) {
+        parseMetalsResponse[Set[api.Problem]](maybeText).map { problems =>
+          val diags = problems.map(CodeEditor.problemToDiagnostic(_, view.state.doc)).toJSArray
+          view.update(js.Array(view.state.update(setDiagnostics(view.state, diags))))
+        }
+      } else None
+    }
   })
 }
