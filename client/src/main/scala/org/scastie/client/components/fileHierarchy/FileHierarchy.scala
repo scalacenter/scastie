935a4d786d48bb5fa4237c17f59c81707f0dab97
Jakub Warchoł <93600787+warcholjakub@users.noreply.github.com>
2026-02-12 16:21:19 +0100

    feat: implement drag and drop file moving in file hierarchy

--- client/src/main/scala/org/scastie/client/components/fileHierarchy/FileHierarchy.scala
+++ client/src/main/scala/org/scastie/client/components/fileHierarchy/FileHierarchy.scala
@@ -1,28 +1,9 @@
 package org.scastie.client.components.fileHierarchy
 
+import org.scastie.client.components.fileHierarchy.FileOrFolderUtils.{find, move, recomputePaths}
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 
-
-trait FileOrFolder {
-  val name: String = ""
-  val isFolder: Boolean
-}
-
-case class File(override val name: String, content: String = "<empty content>") extends FileOrFolder {
-  override val isFolder: Boolean = false
-}
-
-case class Folder(override val name: String, files: List[FileOrFolder] = List()) extends FileOrFolder {
-  override val isFolder: Boolean = true
-}
-
-case class FileHierarchyState(
-  root: Folder,
-  selectedFile: String,
-  dragSource: String = "",
-  dragTarget: String = ""
-)
 
 final case class FileHierarchy(rootFolder: Folder) {
   @inline def render: VdomElement = FileHierarchy.component()
@@ -30,49 +11,51 @@ final case class FileHierarchy(rootFolder: Folder) {
 
 object FileHierarchy {
 
+  case class FileHierarchyState(root: Folder, selectedFile: String, dragSrc: String, dragOver: String)
+
   val initialState = FileHierarchyState(
-    root = Folder("Folder A",
-      List(
-        File("File A.1"),
-        Folder("Folder A.B", List(
-          File("File A.B.1"),
-          File("File A.B.2"),
-          File("FA.B.2"),
-          File("File A.B.3")
-        )),
-        File("File A.2"),
-        File("File A.3"),
-        Folder("File A.C", List(
-          File("File A.C.1")
-        ))
-      )
-    ),
-    selectedFile = "File A.B.1"
-  )
+    root = recomputePaths(
+      Folder("root", isRoot = true)
+        .add2("i.txt")
+        .add2("j.txt")
+        .add2("A/k.txt")
+        .add2("B", isFolder = true)),
+    selectedFile = "root",
+    dragSrc = "",
+    dragOver = "")
 
   val component =
     ScalaFnComponent.withHooks[Unit]
       .useState(initialState)
       .render($ => {
-
-        val fn: String => Callback = {
-          s => $.hook1.modState(st => st.copy(selectedFile = s))
+        val selectFile: String => Callback = {
+          s =>
+            $.hook1.modState(_.copy(selectedFile = s))
         }
-
-        val fn2: DragInfo => Callback = {
+        val dragInfoUpdate: DragInfo => Callback = {
           di =>
             if (di.start && !di.end) {
-              $.hook1.modState(st => st.copy(dragSource = di.f.name)).void
+              $.hook1.modState(_.copy(dragSrc = di.f.path))
             } else if (!di.start && di.end) {
-              Callback.log($.hook1.value.dragSource + " to " + $.hook1.value.dragTarget) // TODO move in file hierarchy
+              $.hook1.modState {
+                case FileHierarchyState(root, selectedFile, dragSrc, dragOver) =>
+                  val srcPath = dragSrc
+                  val dstPath = dragOver
+                  Callback.log(srcPath + " to " + dstPath).runNow()
+
+                  val newRoot = move(root, srcPath, dstPath)
+                  val newSelectedFile = dstPath + "/" + find(root, srcPath).get.name
+                  FileHierarchyState(newRoot, newSelectedFile, dragSrc, dragOver)
+              }.runNow()
+              Callback.empty
             } else if (!di.start && !di.end) {
-              $.hook1.modState(st => st.copy(dragTarget = di.f.name)).void
+              $.hook1.modState(_.copy(dragOver = di.f.path))
             } else {
               Callback.throwException(new IllegalArgumentException())
             }
         }
         <.div(
-          FileOrFolderNode($.hook1.value.root, $.hook1.value.selectedFile, 0, fn, fn2).render
+          FileOrFolderNode($.hook1.value.root, $.hook1.value.selectedFile, 0, selectFile, dragInfoUpdate).render
         )
       })
 }
---
+++ client/src/main/scala/org/scastie/client/components/fileHierarchy/FileOrNode.scala
@@ -0,0 +1,126 @@
+package org.scastie.client.components.fileHierarchy
+
+import org.scastie.client.components.fileHierarchy.FileOrFolderUtils.prependPath
+
+sealed trait FileOrFolder {
+  val name: String
+  val path: String
+  val isFolder: Boolean
+  val isRoot: Boolean
+}
+
+case class File(override val name: String, content: String = "<empty content>", override val path: String = "") extends FileOrFolder {
+  override val isFolder: Boolean = false
+  override val isRoot: Boolean = false
+}
+
+case class Folder(override val name: String, children: List[FileOrFolder] = List(), override val path: String = "", override val isRoot: Boolean = false) extends FileOrFolder {
+  override val isFolder: Boolean = true
+
+  def add(ff: FileOrFolder): Folder = {
+    if (this.path.nonEmpty) {
+      val f = prependPath(this.path, ff)
+      this.copy(children = this.children :+ f)
+    } else {
+      val rootName = name
+      val f = if (isRoot) prependPath(rootName, ff) else ff
+      this.copy(children = this.children :+ f)
+    }
+  }
+
+  def add2(path: String, isFolder: Boolean = false): Folder = {
+    path.split("/").toList match {
+      case Nil => this
+      case head :: Nil =>
+        val newFilePath = this.path + "/" + head
+        val newFile = if (isFolder) Folder(head, path = newFilePath) else File(head, newFilePath)
+        copy(children = children :+ newFile)
+      case head :: tail =>
+        val newIntermediate = Folder(head, path = this.path + "/" + head)
+        val folder = children.find(_.name.equals(head)).getOrElse(newIntermediate).asInstanceOf[Folder]
+        copy(children = children :+ folder.add2(tail.mkString("/"), isFolder))
+    }
+  }
+}
+
+object FileOrFolderUtils {
+
+  def find(root: Folder, path: String): Option[FileOrFolder] = {
+    if (root.path == path) {
+      Some(root)
+    } else {
+      root.children.foldLeft[Option[FileOrFolder]](None) { (acc, fileOrFolder) =>
+        acc.orElse(fileOrFolder match {
+          case f: File if f.path == path => Some(f)
+          case l: Folder => find(l, path)
+          case _ => None
+        })
+      }
+    }
+  }
+
+  def remove(root: Folder, path: String): Folder = {
+    root.copy(children = root.children
+      .map {
+        case f: File => f
+        case l: Folder => remove(l, path)
+      }
+      .filterNot(_.path == path)
+    )
+  }
+
+  def insert(root: Folder, newFolder: FileOrFolder, path: String): Folder = {
+    recomputePaths(
+      root.copy(children =
+        if (root.path.equals(path)) {
+          root.children.filterNot(_.name == newFolder.name) :+ newFolder
+        } else
+          root.children.map {
+            case f: File =>
+              if (f.path.equals(path)) throw new IllegalArgumentException("BRUH")
+              else f
+            case l: Folder => insert(l, newFolder, path)
+          }
+      ))
+  }
+
+  def move(root: Folder, srcPath: String, dstPath: String): Folder = {
+    if (dstPath.startsWith(srcPath)) {
+      root
+    } else {
+      val movingFolder = find(root, srcPath).get
+      try {
+        recomputePaths(
+          insert(
+            remove(root, srcPath),
+            movingFolder,
+            dstPath)
+        )
+      } catch {
+        case x: IllegalArgumentException =>
+          Console.println("Cannot drop into a file :)")
+          root
+      }
+    }
+  }
+
+  def recomputePaths(f: Folder, prefix: String = ""): Folder = {
+    f.copy(
+      path = prefix + "/" + f.name,
+      children = f.children.map {
+        case folder: Folder => recomputePaths(folder, prefix + "/" + f.name)
+        case file: File => file.copy(path = prefix + "/" + f.name + "/" + file.name)
+      })
+  }
+
+  def prependPath(p: String, fileOrFolder: FileOrFolder): FileOrFolder = {
+    fileOrFolder match {
+      case File(name, content, path) =>
+        val nonEmptyPath = if (path.isEmpty) name else path
+        File(name, content, p + "/" + nonEmptyPath)
+      case Folder(name, files, path, ir) =>
+        val nonEmptyPath = if (path.isEmpty) name else path
+        Folder(name, files.map(x => prependPath(p + "/" + name, x)), p + "/" + nonEmptyPath)
+    }
+  }
+}
