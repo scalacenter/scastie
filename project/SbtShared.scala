6ba0f8c7a0e382d6ae4487930409f4b8cdb8de2d
Jakub Warchoł <warcholljakub@gmail.com>
2026-04-08 14:47:26 +0200

    chore: 3.8.4-RC1 bump

--- project/SbtShared.scala
+++ project/SbtShared.scala
@@ -35,7 +35,7 @@ object SbtShared {
     val stableLTS  = "3.3.7"
     val stableNext = "3.8.3"
     val latestLTS  = "3.3.7"
-    val latestNext = "3.8.3"
+    val latestNext = "3.8.4-RC1"
     val js         = latest213
     val sbt        = latest212
     val jvm        = latest213
